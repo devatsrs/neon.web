9fddee48703db581ad28b0f3e5defa3e87b3b554
deven <shriramsoft@gmail.com>
2016-02-15 19:16:46 +0530

    Payment upload db

---
+++ dbv-2/data/schema/prc_insertPayments.sql
@@ -0,0 +1,49 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_insertPayments`(IN `p_CompanyID` INT, IN `p_ProcessID` VARCHAR(100), IN `p_UserID` INT)
+BEGIN
+
+	DECLARE v_UserName varchar(30);
+ 	
+ 	SELECT CONCAT(u.FirstName,CONCAT(' ',u.LastName)) as name into v_UserName from Ratemanagement3.tblUser u where u.UserID=p_UserID;
+ 	
+ 	INSERT INTO tblPayment (
+	 		CompanyID,
+	 		 AccountID,
+			 InvoiceNo,
+			 PaymentDate,
+			 PaymentMethod,
+			 PaymentType,
+			 Notes,
+			 Amount,
+			 Currency,
+			 Recall,
+			 `Status`,
+			 created_at,
+			 updated_at,
+			 CreatedBy,
+			 ModifyBy,
+			 RecallReasoan,
+			 RecallBy)
+ 	select distinct tp.CompanyID,
+	 		 tp.AccountID,
+			 tp.InvoiceNo,
+			 tp.PaymentDate,
+			 tp.PaymentMethod,
+			 tp.PaymentType,
+			 tp.Notes,
+			 tp.Amount,
+			 (select `Code` 
+					from Ratemanagement3.tblAccount ac 
+					inner join Ratemanagement3.tblCurrency cr on ac.CurrencyId = cr.CurrencyId 
+					where (ac.AccountID = tp.AccountID) limit 1 ) as Currency,
+			 0 as Recall,
+			 tp.Status,
+			 Now() as created_at,
+			 Now() as updated_at,
+			 v_UserName as CreatedBy,
+			 '' as ModifyBy,
+			 '' as RecallReasoan,
+			 '' as RecallBy
+	from tblTempPayment tp
+	where tp.ProcessID = p_ProcessID
+			AND tp.CompanyID = p_CompanyID;	
+END
---
+++ dbv-2/data/schema/prc_validatePayments.sql
@@ -0,0 +1,47 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_validatePayments`(IN `p_CompanyID` INT, IN `p_ProcessID` VARCHAR(50))
+BEGIN
+
+SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+
+	DROP TEMPORARY TABLE IF EXISTS tmp_error_;
+	CREATE TEMPORARY TABLE tmp_error_(
+		`ErrorMessage` VARCHAR(500)
+	);
+
+	INSERT INTO tmp_error_
+	SELECT  CONCAT('Duplicate Payment in file Against Account ',IFNULL(ac.AccountName,''),' Invoice Number:' ,IFNULL(pt.InvoiceNo,''),' Payment Date:',IFNULL(pt.PaymentDate,'')) as ErrorMessage  FROM tblTempPayment pt
+	INNER JOIN Ratemanagement3.tblAccount ac on ac.AccountID = pt.AccountID
+	INNER JOIN tblTempPayment tp on tp.ProcessID = pt.ProcessID
+		AND tp.AccountID = pt.AccountID
+		AND tp.PaymentDate = pt.PaymentDate
+		AND tp.Amount = pt.Amount
+		AND tp.PaymentType = pt.PaymentType
+		AND tp.PaymentMethod = pt.PaymentMethod
+	WHERE pt.CompanyID = p_CompanyID
+		AND pt.ProcessID = p_ProcessID
+ 	GROUP BY pt.InvoiceNo,pt.AccountID,pt.PaymentDate,pt.PaymentType,pt.PaymentMethod,pt.Amount 
+	HAVING COUNT(*)>1;
+ 	
+	INSERT INTO tmp_error_	
+	SELECT
+		CONCAT('Duplicate Payment in system Against Account ',IFNULL(ac.AccountName,''),' Invoice Number:' ,IFNULL(pt.InvoiceNo,''),' Payment Date:',IFNULL(pt.PaymentDate,'')) as  ErrorMessage
+	FROM tblTempPayment pt
+	INNER JOIN Ratemanagement3.tblAccount ac on ac.AccountID = pt.AccountID
+	INNER JOIN tblPayment p on p.CompanyID = pt.CompanyID 
+		AND p.AccountID = pt.AccountID
+		AND p.PaymentDate = pt.PaymentDate
+		AND p.Amount = pt.Amount
+		AND p.PaymentType = pt.PaymentType
+	WHERE pt.CompanyID = p_CompanyID
+		AND pt.ProcessID = p_ProcessID;
+	
+	
+	IF (SELECT COUNT(*) FROM tmp_error_) > 0
+	THEN
+	SELECT GROUP_CONCAT(ErrorMessage SEPARATOR "\r\n" ) as ErrorMessage FROM	tmp_error_;
+		
+	END IF;
+	
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END
