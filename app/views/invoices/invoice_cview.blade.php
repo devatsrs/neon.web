f0972c31e5e90665b42bde02e1f0e7d3276850eb
girish vadher <girish.vadher@code-desk.com>
2016-03-07 15:51:02 +0530

    billing subscription currency symbol

--- app/views/invoices/invoice_cview.blade.php
+++ app/views/invoices/invoice_cview.blade.php
@@ -26,7 +26,7 @@
                         <div class="due">@if($Invoice->InvoiceStatus == Invoice::PAID) Paid @else DUE @endif</div>
                     </div>
                     <div class="amount">
-                        <span class="overdue"><em class="currency overdue">{{$CurrencySymbol}}</em>{{number_format($Invoice->GrandTotal,$Account->RoundChargesAmount)}}</span>
+                        <span class="overdue">{{$CurrencySymbol}}{{number_format($Invoice->GrandTotal,$Account->RoundChargesAmount)}}</span>
                     </div>
                 </div>
                 <div class="x-span4 pull-left" > <h1 class="text-center">Invoice</h1></div>
