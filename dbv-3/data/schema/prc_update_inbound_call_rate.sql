b86f317acd92396ad29b95bf4c901d30494abc4d
deven <shriramsoft@gmail.com>
2016-03-07 12:41:52 +0530

    Inbound ReRate fix

--- dbv-3/data/schema/prc_update_inbound_call_rate.sql
+++ dbv-3/data/schema/prc_update_inbound_call_rate.sql
@@ -10,7 +10,6 @@ BEGIN
 	);
 	DROP TEMPORARY TABLE IF EXISTS tmp_Message_;
    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_Message_(
-   		TempUsageDetailID int,
 			Message Text
 	);
 	
@@ -71,7 +70,7 @@ BEGIN
    
    /* Return Error message   */
   	/*set @stm4 = CONCAT('insert into tmp_Message_ 
-	  						select  TempUsageDetailID, concat( "Account doesnt match for " , GatewayAccountID )
+	  						select  concat( "Account doesnt match for " , GatewayAccountID )
 							  from  ' , p_tbltempusagedetail_name  , ' where ProcessID = "' , p_processid  , '" and is_inbound = 1 and AccountID is null ');
 
 	PREPARE stmt4 FROM @stm4;
@@ -80,7 +79,7 @@ BEGIN
 	*/
 
   	set @stm5 = CONCAT('insert into tmp_Message_ 
-								select   distinct ud.TempUsageDetailID , concat( "Prefix not found for " , ud.GatewayAccountID )
+								select   distinct concat( "Prefix not found for " , ud.GatewayAccountID )
 							  from  ' , p_tbltempusagedetail_name  , ' ud
 							  inner join Ratemanagement3.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.AccountID  = ud.AccountID and a.InboudRateTableID > 0 
 							  where ud.ProcessID = "' , p_processid  , '" and ud.is_inbound = 1 and ud.area_prefix = "Other"  ');
