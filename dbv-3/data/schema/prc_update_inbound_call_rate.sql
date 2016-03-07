b81e2ae82e40422ed634810f5e1071add25160f0
deven <shriramsoft@gmail.com>
2016-03-07 12:25:13 +0530

    Inbound ReRate fix

--- dbv-3/data/schema/prc_update_inbound_call_rate.sql
+++ dbv-3/data/schema/prc_update_inbound_call_rate.sql
@@ -34,7 +34,8 @@ BEGIN
     PREPARE stmt1 FROM @stm1;
     EXECUTE stmt1;
     DEALLOCATE PREPARE stmt1;
-   	 
+  
+     	 
     
     set @stm2 = CONCAT('UPDATE ' , p_tbltempusagedetail_name , ' tbl2
     INNER JOIN tmp_TempUsageDetail_ tbl
@@ -43,7 +44,7 @@ BEGIN
     WHERE tbl2.processId = "' , p_processid , '" and tbl2.is_inbound = 1 ;
 	 ');
    	
-
+     
     PREPARE stmt2 FROM @stm2;
     EXECUTE stmt2;
     DEALLOCATE PREPARE stmt2;
@@ -69,18 +70,20 @@ BEGIN
    DEALLOCATE PREPARE stmt3;
    
    /* Return Error message   */
-  	set @stm4 = CONCAT('insert into tmp_Message_ 
+  	/*set @stm4 = CONCAT('insert into tmp_Message_ 
 	  						select  TempUsageDetailID, concat( "Account doesnt match for " , GatewayAccountID )
 							  from  ' , p_tbltempusagedetail_name  , ' where ProcessID = "' , p_processid  , '" and is_inbound = 1 and AccountID is null ');
 
 	PREPARE stmt4 FROM @stm4;
    EXECUTE stmt4;
    DEALLOCATE PREPARE stmt4;
-
+	*/
 
   	set @stm5 = CONCAT('insert into tmp_Message_ 
-								select  TempUsageDetailID, concat( "Prefix not found for " , GatewayAccountID )
-							  from  ' , p_tbltempusagedetail_name  , ' where ProcessID = "' , p_processid  , '" and is_inbound = 1 and area_prefix = "Other"  ');
+								select   distinct ud.TempUsageDetailID , concat( "Prefix not found for " , ud.GatewayAccountID )
+							  from  ' , p_tbltempusagedetail_name  , ' ud
+							  inner join Ratemanagement3.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.AccountID  = ud.AccountID and a.InboudRateTableID > 0 
+							  where ud.ProcessID = "' , p_processid  , '" and ud.is_inbound = 1 and ud.area_prefix = "Other"  ');
 
 	PREPARE stmt5 FROM @stm5;
    EXECUTE stmt5;
