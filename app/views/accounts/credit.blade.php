c12d5a78bb00c7ed9984a6b38b9800682289285c
girish vadher <girish.vadher@code-desk.com>
2016-03-16 12:46:25 +0530

    view file missed

---
+++ app/views/accounts/credit.blade.php
@@ -0,0 +1,139 @@
+@extends('layout.main')
+
+@section('content')
+
+<ol class="breadcrumb bc-3">
+    <li>
+        <a href="{{action('dashboard')}}"><i class="entypo-home"></i>Home</a>
+    </li>
+    <li>
+        <a href="{{URL::to('accounts')}}"> </i>Accounts</a>
+    </li>
+    <li>
+        <a href="{{URL::to('accounts/'.$account->AccountID.'/edit')}}"></i>Edit Account({{$account->AccountName}})</a>
+    </li>
+    <li class="active">
+
+        <strong>Account Credits</strong>
+    </li>
+</ol>
+<h3>Account Credits</h3>
+<p style="text-align: right;">
+
+    <button type="button" id="save_account" class="save btn btn-primary btn-sm btn-icon icon-left" data-loading-text="Loading...">
+        <i class="entypo-floppy"></i>
+        Save
+    </button>
+
+
+    <a href="{{URL::to('accounts/'.$account->AccountID.'/edit')}}" class="btn btn-danger btn-sm btn-icon icon-left">
+        <i class="entypo-cancel"></i>
+        Close
+    </a>
+</p>
+
+<div class="row">
+    <div class="col-md-12">
+        <form novalidate="novalidate" class="form-horizontal form-groups-bordered validate" method="post" id="customer_detail">
+            <div data-collapsed="0" class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="panel-title">
+                        Customer Details
+                    </div>
+                    <div class="panel-options">
+                        <a data-rel="collapse" href="#"><i class="entypo-down-open"></i></a>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <div class="form-group">
+
+                        <label for="field-1" class="col-sm-2 control-label">Permanent Credit Limit</label>
+                        <div class="desc col-sm-4 ">
+                            <input type="text" class="form-control"  name="PermanentCredit" value="" id="PermanentCredit Limit">
+                        </div>
+                        </div><div class="form-group">
+                        <label for="field-1" class="col-sm-2 control-label">Temporary Credit Limit</label>
+                        <div class="desc col-sm-4 ">
+                            <input type="text" class="form-control"  name="TemporaryCredit" value="" id="PermanentCredit Limit">
+                        </div>
+                    </div><div class="form-group">
+                        <label for="field-1" class="col-sm-2 control-label">Balance Warning</label>
+                        <div class="desc col-sm-4 ">
+                            <input type="text" class="form-control"  name="BalanceThreshold" value="" id="Threshold Limit">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<table class="table table-bordered datatable" id="table-4">
+    <thead>
+    <tr>
+        <th width="10%" >Permanent Credit</th>
+        <th width="15%" >Temporary Credit</th>
+        <th width="10%" >Balance Warning</th>
+        <th width="10%" >Created By</th>
+        <th width="10%" >Created at</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        var acountiptable;
+        $('#save_account').click(function(){
+            var post_data = $('#vendor_detail').serialize()+'&'+$('#customer_detail').serialize()+'&AccountID='+'{{$account->AccountID}}';
+            var post_url = '{{URL::to('account/update_credit')}}';
+            submit_ajax(post_url,post_data);
+        });
+        data_table = $("#table-4").dataTable({
+            "bDestroy": true,
+            "bProcessing":true,
+            "bServerSide":true,
+            "sAjaxSource": baseurl + "/account/ajax_datagrid_credit",
+            "iDisplayLength": '{{Config::get('app.pageSize')}}',
+            "sPaginationType": "bootstrap",
+            "sDom": "<'row'<'col-xs-6 col-left'l><'col-xs-6 col-right'<'export-data'T>f>r>t<'row'<'col-xs-6 col-left'i><'col-xs-6 col-right'p>>",
+            "fnServerParams": function(aoData) {
+                aoData.push(
+                        {"name":"AccountID","value":'{{$account->AccountID}}'}
+                );
+                data_table_extra_params.length = 0;
+                data_table_extra_params.push(
+                        {"name":"Export","value":1}
+                );
+            },
+            "aaSorting": [[0, 'asc']],
+            "aoColumns":
+                    [
+                        {  "bSortable": true },
+                        {  "bSortable": true },
+                        {  "bSortable": true },
+                        {  "bSortable": true },
+                        {  "bSortable": true }
+
+                    ],
+            "oTableTools": {
+                "aButtons": [
+                    {
+                        "sExtends": "download",
+                        "sButtonText": "Export Data",
+                        "sUrl": baseurl + "/cronjobs/ajax_datagrid", //baseurl + "/generate_xls.php",
+                        sButtonClass: "save-collection"
+                    }
+                ]
+            },
+            "fnDrawCallback": function() {
+                $(".dataTables_wrapper select").select2({
+                    minimumResultsForSearch: -1
+                });
+            }
+
+        });
+    });
+
+</script>
+@stop
