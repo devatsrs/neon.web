0f3537b16ec12901fdc02527d21163b69e7cb4e5
umer <umer.ahmed@code-desk.com>
2016-03-23 16:07:32 +0500

    Estimate email changes for estimate pdf and email layouts

--- app/views/estimates/estimates_cview.blade.php
+++ app/views/estimates/estimates_cview.blade.php
@@ -29,8 +29,8 @@
         <div class="x-content">
             <div class="x-row">
                 <div class="x-span8">                   
-                    <div class="amount">
-                        <span class="overdue">{{number_format($Estimate->GrandTotal,$Account->RoundChargesAmount)}} <em class="currency overdue">{{$CurrencyCode}}</em></span>
+                    <div class="amount">                        
+                         <span class="overdue">{{$CurrencySymbol}}{{number_format($Estimate->GrandTotal,$Account->RoundChargesAmount)}}</span>
                     </div>
                 </div>
                 <div class="x-span4 pull-left" > <h1 class="text-center">Estimate</h1></div>
