b9a020489a711eb8bdb9fa599cb03ffe8d9436d8
girish vadher <girish.vadher@code-desk.com>
2016-04-01 18:38:30 +0530

    bug fixed in procedures

--- dbv-2/data/schema/prc_updatePrefixTrunk.sql
+++ dbv-2/data/schema/prc_updatePrefixTrunk.sql
@@ -1,26 +1,21 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_updatePrefixTrunk`(IN `p_CompanyID` int, IN `p_CompanyGatewayID` int, IN `p_processId` varchar(200), IN `p_tbltempusagedetail_name` VARCHAR(200))
-	LANGUAGE SQL
-	NOT DETERMINISTIC
-	CONTAINS SQL
-	SQL SECURITY DEFINER
-	COMMENT ''
 BEGIN
-
+	
 	SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-/*
+
       -- update trunk with first trunk if not set UseInBilling
    set @stm1 = CONCAT(' UPDATE LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud
-    LEFT JOIN tblGatewayAccount  ga
+    LEFT JOIN tblGatewayAccount  ga 
         ON ud.GatewayAccountID = ga.GatewayAccountID
         AND ud.CompanyGatewayID = ga.CompanyGatewayID
         AND ud.CompanyID = ga.CompanyID and ud.processId = "' , p_processId , '"
-    LEFT JOIN LocalRatemanagement.tblCustomerTrunk ct
-        ON ct.AccountID = ga.AccountID AND ct.Status =1
+    LEFT JOIN LocalRatemanagement.tblCustomerTrunk ct 
+        ON ct.AccountID = ga.AccountID AND ct.Status =1 
         AND UseInBilling = 0 and ud.processId = "' , p_processId , '"
-    LEFT JOIN LocalRatemanagement.tblTrunk t
+    LEFT JOIN LocalRatemanagement.tblTrunk t 
         ON t.TrunkID = ct.TrunkID and ud.processId = "' , p_processId , '"
 	 SET ud.trunk = IFNULL(t.Trunk,"Other")
-    WHERE
+    WHERE 
     ud.processId = "' , p_processId , '"
     AND ud.CompanyID = "' , p_CompanyID , '"
     AND ud.CompanyGatewayID = "' , p_CompanyGatewayID , '"
@@ -30,22 +25,22 @@ BEGIN
     PREPARE stmt1 FROM @stm1;
     EXECUTE stmt1;
     DEALLOCATE PREPARE stmt1;
-	*/
+	
 
-/*
+
  -- update trunk if set UseInBilling
      set @stm2 = CONCAT(' UPDATE LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud
-    LEFT JOIN tblGatewayAccount  ga
+    LEFT JOIN tblGatewayAccount  ga 
         ON ud.GatewayAccountID = ga.GatewayAccountID
         AND ud.CompanyGatewayID = ga.CompanyGatewayID
         AND ud.CompanyID = ga.CompanyID and ud.processId = "' , p_processId , '"
-    LEFT JOIN LocalRatemanagement.tblCustomerTrunk ct
+    LEFT JOIN LocalRatemanagement.tblCustomerTrunk ct 
         ON ct.AccountID = ga.AccountID AND ct.Status =1 and ud.processId = "' , p_processId , '"
         AND UseInBilling = 1 AND cld LIKE CONCAT(ct.Prefix , "%")
-    LEFT JOIN LocalRatemanagement.tblTrunk t
+    LEFT JOIN LocalRatemanagement.tblTrunk t 
         ON t.TrunkID = ct.TrunkID and ud.processId = "' , p_processId , '"
 	 SET ud.trunk = IFNULL(t.Trunk,"Other")
-    WHERE
+    WHERE 
     ud.processId = "' , p_processId , '"
 	 AND  ud.CompanyID = "' , p_CompanyID , '"
     AND ud.CompanyGatewayID = "' , p_CompanyGatewayID , '"
@@ -56,98 +51,65 @@ BEGIN
     PREPARE stmt2 FROM @stm2;
     EXECUTE stmt2;
     DEALLOCATE PREPARE stmt2;
-    */
-
-
+    
    DROP TEMPORARY TABLE IF EXISTS tmp_TempUsageDetail_;
    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_TempUsageDetail_(
 			TempUsageDetailID int,
-			prefix varchar(50),
-            trunk varchar(50)
+			prefix varchar(50)
 	);
-	DROP TEMPORARY TABLE IF EXISTS tmp_TempUsageDetail2_;
-   CREATE TEMPORARY TABLE IF NOT EXISTS tmp_TempUsageDetail2_(
-            TempUsageDetailID int,
-            prefix varchar(50),
-            trunk varchar(50)
-    );
-
-
-
-   /* insert customer rate prefix into table */
-    set @stmt22 = CONCAT('
-         INSERT INTO tmp_TempUsageDetail_
-        SELECT
-            ud.TempUsageDetailID,
-            MAX(r.Code) AS prefix,
-            IFNULL(MAX(t.Trunk),"Other") as trunk
-            FROM LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud
-            Inner JOIN tblGatewayAccount ga
-                    ON ud.GatewayAccountID = ga.GatewayAccountID
-                    AND ud.CompanyGatewayID = ga.CompanyGatewayID
-                    AND ud.CompanyID = ga.CompanyID and ud.processId = "' , p_processId , '"
-            Inner JOIN LocalRatemanagement.tblCustomerTrunk ct
-                  ON ct.AccountID = ga.AccountID AND ct.Status =1 AND ((ct.UseInBilling = 1 AND cld LIKE CONCAT(ct.Prefix , "%")) OR ct.UseInBilling = 0 )
-            Inner JOIN LocalRatemanagement.tblTrunk t ON t.TrunkID = ct.TrunkID
-            Inner JOIN LocalRatemanagement.tblCustomerRate cr
-                        ON cr.CustomerID = ga.AccountID AND  cr.TrunkID = ct.TrunkID
-            Inner join LocalRatemanagement.tblRate r on r.CompanyId = ', p_companyid ,' and r.CodeDeckId = ct.CodeDeckId and cr.RateID = r.RateID and cr.EffectiveDate <= Now()
-            where ud.ProcessID = "' , p_processid  , '" and ud.cld like concat( r.Code , "%" )
-            group by ud.TempUsageDetailID ;
-        ');
-
-    PREPARE stmt22 FROM @stmt22;
-    EXECUTE stmt22;
-    DEALLOCATE PREPARE stmt22;
-
-
-   /* insert rate table prefix into table */
-    set @stm3 = CONCAT('
-     INSERT INTO tmp_TempUsageDetail2_
-        SELECT
-              ud.TempUsageDetailID,
-            MAX(r.Code) AS prefix,
-            IFNULL(MAX(t.Trunk),"Other") as trunk
-            FROM LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud
-            LEFT join tmp_TempUsageDetail_ tmpcr on ud.processId = "' , p_processId , '" and tmpcr.TempUsageDetailID = ud.TempUsageDetailID
-            Inner JOIN tblGatewayAccount ga
-                    ON ud.GatewayAccountID = ga.GatewayAccountID
-                    AND ud.CompanyGatewayID = ga.CompanyGatewayID
-                    AND ud.CompanyID = ga.CompanyID and ud.processId = "' , p_processId , '"
-            Inner JOIN LocalRatemanagement.tblCustomerTrunk ct
-                  ON ct.AccountID = ga.AccountID AND ct.Status =1 AND ((ct.UseInBilling = 1 AND cld LIKE CONCAT(ct.Prefix , "%")) OR ct.UseInBilling = 0 )
-            Inner JOIN LocalRatemanagement.tblTrunk t ON t.TrunkID = ct.TrunkID
-            Inner join LocalRatemanagement.tblRateTable rt on rt.CompanyId = ', p_companyid ,' and  rt.RateTableId = ct.RateTableId
-            Inner join LocalRatemanagement.tblRate r on r.CompanyId = ', p_companyid ,' and r.CodeDeckId = rt.CodeDeckId
-            Inner join LocalRatemanagement.tblRateTableRate rtr on  rtr.RateTableId =  rt.RateTableId and rtr.RateID = r.RateID and rtr.EffectiveDate <= Now()
-            where tmpcr.TempUsageDetailID  is null and ud.ProcessID = "' , p_processid  , '" and ud.cld like concat( r.Code , "%" )
-            group by ud.TempUsageDetailID;
-    ');
-
-
+	
+	
+	set @stm3 = CONCAT('
+	 INSERT INTO tmp_TempUsageDetail_
+    SELECT
+		  TempUsageDetailID,
+        MAX(r.Code) AS prefix
+    FROM LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud
+    LEFT JOIN tblGatewayAccount ga 
+        ON ud.GatewayAccountID = ga.GatewayAccountID
+        AND ud.CompanyGatewayID = ga.CompanyGatewayID
+        AND ud.CompanyID = ga.CompanyID and ud.processId = "' , p_processId , '"
+    LEFT JOIN LocalRatemanagement.tblCustomerTrunk ct 
+        ON ct.AccountID = ga.AccountID AND ct.Status =1  
+        AND ((ct.UseInBilling = 1 AND cld LIKE CONCAT(ct.Prefix , "%")) OR ct.UseInBilling = 0 ) and ud.processId = "' , p_processId , '"
+    LEFT JOIN LocalRatemanagement.tblCustomerRate cr 
+        ON cr.CustomerID = ga.AccountID
+        AND  cr.TrunkID = ct.TrunkID and ud.processId = "' , p_processId , '"
+        AND cr.EffectiveDate <= Now()
+   LEFT JOIN LocalRatemanagement.tblRateTableRate rtr 
+        ON rtr.RateTableId = ct.RateTableID and ud.processId = "' , p_processId , '"
+        AND rtr.EffectiveDate <= Now()
+    LEFT JOIN LocalRatemanagement.tblRate r 
+        ON ( cr.RateID = r.RateID OR rtr.RateID = r.RateID) and ud.processId = "' , p_processId , '"
+    WHERE  
+ 	 ud.processId = "' , p_processId , '"
+	 AND ud.CompanyID = "' , p_CompanyID , '"
+    AND ud.CompanyGatewayID = "' , p_CompanyGatewayID , '"
+    AND (ud.billed_duration >0 OR ud.cost > 0)
+    AND (
+            (ct.UseInBilling = 1 AND ( (ct.AccountID is not null and  ct.Prefix is null and  cld LIKE CONCAT(r.Code , "%")) or (ct.Prefix is not null and  cld LIKE CONCAT(ct.Prefix,r.Code , "%"))))
+            or 
+            (ct.UseInBilling = 0 AND ( (ct.AccountID is not null and cld LIKE CONCAT(r.Code , "%")) or (cld LIKE CONCAT(r.Code , "%")) ) ) 
+        )
+    GROUP BY  TempUsageDetailID;
+	');
 
     PREPARE stmt3 FROM @stm3;
     EXECUTE stmt3;
     DEALLOCATE PREPARE stmt3;
-
-
-    /* merge tables */
-    Insert into tmp_TempUsageDetail_ (TempUsageDetailID,prefix,trunk  )
-    select (TempUsageDetailID,prefix,trunk     )  from tmp_TempUsageDetail2_;
-
-
-
+   
+	
 	set @stm4 = CONCAT('UPDATE LocalRMCdr.' , p_tbltempusagedetail_name , ' tbl2
-    INNER JOIN tmp_TempUsageDetail_ tbl ON tbl2.TempUsageDetailID = tbl.TempUsageDetailID
-    SET tbl2.area_prefix = tbl.prefix,
-    tbl2.trunk = tbl.trunk
+    INNER JOIN tmp_TempUsageDetail_ tbl
+        ON tbl2.TempUsageDetailID = tbl.TempUsageDetailID
+    SET area_prefix = prefix
     WHERE tbl2.processId = "' , p_processId , '"
 	 ');
 
     PREPARE stmt4 FROM @stm4;
     EXECUTE stmt4;
     DEALLOCATE PREPARE stmt4;
-
-	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+	 
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
         
 END
--- dbv-3/data/schema/prc_update_inbound_call_rate.sql
+++ dbv-3/data/schema/prc_update_inbound_call_rate.sql
@@ -1,17 +1,13 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_update_inbound_call_rate`(IN `p_companyid` INT, IN `p_processid` VARCHAR(100), IN `p_tbltempusagedetail_name` VARCHAR(100))
-	LANGUAGE SQL
-	NOT DETERMINISTIC
-	CONTAINS SQL
-	SQL SECURITY DEFINER
-	COMMENT ''
 BEGIN
 
  	DECLARE v_Account int;
-
+	
 	DROP TEMPORARY TABLE IF EXISTS tmp_TempUsageDetailtech_;
    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_TempUsageDetailtech_(
 			TempUsageDetailID int,
-			prefix varchar(50)
+			prefix varchar(50),
+			INDEX tmpIX_TempUsageDetailtech_TempUsageDetailID (`TempUsageDetailID`)
 	);
 	DROP TEMPORARY TABLE IF EXISTS tmp_Message_;
    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_Message_(
@@ -20,105 +16,105 @@ BEGIN
 
 	/* Check if Inboud Rate Table Selected Against any account  */
     Select  count(*) into v_Account from LocalRatemanagement.tblAccount  where  CompanyId = p_companyid  and InboudRateTableID > 0 ;
-
-
+	
+ 
 	/*--- Update area_prefix */
 	-- IF ( v_Account > 0 AND p_RateCDR =1)
    -- IF ( p_RateCDR =1 )
 	-- THEN
-
+	
 		set @stm1 = CONCAT('
 		 INSERT INTO tmp_TempUsageDetailtech_
 	    SELECT
 			  ud.TempUsageDetailID,
 	        MAX(r.Code) AS prefix
 			FROM `' , p_tbltempusagedetail_name , '` ud
-			inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.InboudRateTableID > 0 and a.AccountID  = ud.AccountID
+			inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.InboudRateTableID > 0 and a.AccountID  = ud.AccountID 
 			Inner join LocalRatemanagement.tblRateTable rt on rt.CompanyId = ', p_companyid ,' and  rt.RateTableId = a.InboudRateTableID
-			Inner join LocalRatemanagement.tblRate r on r.CompanyId = ', p_companyid ,' and r.CodeDeckId = rt.CodeDeckId
-			Inner join LocalRatemanagement.tblRateTableRate rtr on  rtr.RateTableId =  rt.RateTableId and rtr.RateID = r.RateID
+			Inner join LocalRatemanagement.tblRate r on r.CompanyId = ', p_companyid ,' and r.CodeDeckId = rt.CodeDeckId 
+			Inner join LocalRatemanagement.tblRateTableRate rtr on  rtr.RateTableId =  rt.RateTableId and rtr.RateID = r.RateID  
 			where ud.ProcessID = "' , p_processid  , '" and ud.is_inbound = 1 and ud.cld like concat( r.Code , "%" )
 			group by ud.TempUsageDetailID ;
 		');
-
-
+	
+				
 	    PREPARE stmt1 FROM @stm1;
 	    EXECUTE stmt1;
 	    DEALLOCATE PREPARE stmt1;
-
-
-
+	  
+	     	 
+	    
 	    set @stm2 = CONCAT('UPDATE ' , p_tbltempusagedetail_name , ' tbl2
 	    INNER JOIN tmp_TempUsageDetailtech_ tbl
 	        ON tbl2.TempUsageDetailID = tbl.TempUsageDetailID
-	    SET tbl2.area_prefix = tbl.prefix
+	    SET tbl2.area_prefix = tbl.prefix 
 	    WHERE tbl2.processId = "' , p_processid , '" and tbl2.is_inbound = 1 ;
 		 ');
-
-
+	   	
+	     
 	    PREPARE stmt2 FROM @stm2;
 	    EXECUTE stmt2;
 	    DEALLOCATE PREPARE stmt2;
-
+	    
 		/* Update Inbound Rate  */
-
+		
 		set @stm3 = CONCAT('update  ' , p_tbltempusagedetail_name  , ' ud
-			inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.InboudRateTableID > 0 and a.AccountID  = ud.AccountID
+			inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.InboudRateTableID > 0 and a.AccountID  = ud.AccountID 
 			Inner join LocalRatemanagement.tblRateTable rt on rt.CompanyId = ', p_companyid ,' and  rt.RateTableId = a.InboudRateTableID
 			Inner join LocalRatemanagement.tblRate r on r.CompanyId = ', p_companyid ,' and r.CodeDeckId = rt.CodeDeckId and r.Code = ud.area_prefix
-			Inner join LocalRatemanagement.tblRateTableRate rtr on  rtr.RateTableId =  rt.RateTableId and rtr.RateID = r.RateID
+			Inner join LocalRatemanagement.tblRateTableRate rtr on  rtr.RateTableId =  rt.RateTableId and rtr.RateID = r.RateID  
 		SET ud.cost =  CASE WHEN  ud.billed_duration >= rtr.Interval1
 		THEN
 			(rtr.Rate/60.0)*rtr.Interval1+CEILING((ud.billed_duration-rtr.Interval1)/rtr.IntervalN)*(rtr.Rate/60.0)*rtr.IntervalN+ifnull(rtr.ConnectionFee,0)
-		ElSE
+		ElSE 
 			CASE WHEN  billed_duration > 0
-		    THEN
+		    THEN	   
             rtr.Rate+ifnull(rtr.ConnectionFee,0)
           ELSE
 			    0
-		    END
-
-
-		END
+		    END		    
+		    
+			
+		END 
 		where ud.ProcessID = "' , p_processid  , '" and ud.is_inbound = 1  ;');
-
+	
 		PREPARE stmt3 FROM @stm3;
 	   EXECUTE stmt3;
 	   DEALLOCATE PREPARE stmt3;
-
-
+	   
+	   
       /* set cost = 0 where prefix not matched in p_tbltempusagedetail_name table  */
 	   set @stm4 = CONCAT('UPDATE ' , p_tbltempusagedetail_name , ' tbl2
 	   LEFT JOIN tmp_TempUsageDetailtech_ tbl
 	       ON tbl2.TempUsageDetailID = tbl.TempUsageDetailID
 	   SET tbl2.cost = 0
 	   WHERE tbl2.processId = "' , p_processid , '" and tbl2.is_inbound = 1 and tbl.prefix is null  ;');
-
+	     
 	   PREPARE stmt4 FROM @stm4;
 	   EXECUTE stmt4;
 	   DEALLOCATE PREPARE stmt4;
-
+	   
 	   /* Return Error message   */
-	  	/*set @stm4 = CONCAT('insert into tmp_Message_
+	  	/*set @stm4 = CONCAT('insert into tmp_Message_ 
 		  						select  concat( "Account doesnt match for " , GatewayAccountID )
 								  from  ' , p_tbltempusagedetail_name  , ' where ProcessID = "' , p_processid  , '" and is_inbound = 1 and AccountID is null ');
-
+	
 		PREPARE stmt4 FROM @stm4;
 	   EXECUTE stmt4;
 	   DEALLOCATE PREPARE stmt4;
 		*/
-
-	  	set @stm5 = CONCAT('insert into tmp_Message_
+	
+	  	set @stm5 = CONCAT('insert into tmp_Message_ 
 									select   distinct concat( "Account :: " , a.AccountName ,  "  Rate Code :: ",ud.cld)
 								  from  ' , p_tbltempusagedetail_name  , ' ud
-								  inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.AccountID  = ud.AccountID and a.InboudRateTableID > 0
+								  inner join LocalRatemanagement.tblAccount a on  a.CompanyId = ', p_companyid ,' and a.AccountID  = ud.AccountID and a.InboudRateTableID > 0 
 								  where ud.ProcessID = "' , p_processid  , '" and ud.is_inbound = 1 and ud.area_prefix = "Other"  ');
-
+	
 		PREPARE stmt5 FROM @stm5;
 	   EXECUTE stmt5;
 	   DEALLOCATE PREPARE stmt5;
 
-
+	
 	-- END IF;
 	
 	select distinct Message from tmp_Message_;
