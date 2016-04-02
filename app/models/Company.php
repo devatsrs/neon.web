96f36a869dac888f0826efd54c2c88baffe6782d
bhavin machhoya <bhavin@code-desk.com>
2016-04-02 18:20:36 +0530

    company name in license change

--- app/models/Company.php
+++ app/models/Company.php
@@ -72,7 +72,7 @@ class Company extends \Eloquent {
         $result['LicenceProperties'] = '';
         $company_id = User::get_companyID();
         //$result['CompanyName'] = company::getName($company_id);
-        $result['CompanyName'] = getenv('CompanyName');
+        $result['CompanyName'] = getenv('COMPANY_NAME');
         //$result['CompanyName'] = 'abc';
         if(!empty($LICENCE_KEY)) {
 
