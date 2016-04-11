6a64bf3cb059f49dc88410dc9402117054bff19e
girish vadher <girish.vadher@code-desk.com>
2016-04-11 11:08:32 +0530

    sipp and vos files issue fixed

--- dbv-2/data/schema/tblUsageDownloadFiles.sql
+++ dbv-2/data/schema/tblUsageDownloadFiles.sql
@@ -6,6 +6,10 @@ CREATE TABLE `tblUsageDownloadFiles` (
   `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `CreatedBy` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `UpdatedBy` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Status` int(11) DEFAULT '1' COMMENT '1=pending;2=progress;3=completed;4=error;',
+  `processcount` int(11) DEFAULT '0',
+  `process_at` datetime DEFAULT NULL,
+  `message` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`UsageDownloadFilesID`),
   UNIQUE KEY `IX_gateway_filename` (`CompanyGatewayID`,`filename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
