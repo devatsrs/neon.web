35a6ccb19ee99de3fc50032abd681647c69f5c45
bhavin machhoya <bhavin@code-desk.com>
2016-04-19 19:02:51 +0530

    Leads select all button position set

--- app/views/leads/index.blade.php
+++ app/views/leads/index.blade.php
@@ -727,6 +727,17 @@
 
 
 </script>
+<style>
+    .dataTables_filter label{
+        display:none !important;
+    }
+    .dataTables_wrapper .export-data{
+        right: 30px !important;
+    }
+    #selectcheckbox{
+        padding: 15px 10px;
+    }
+</style>
 <link rel="stylesheet" href="assets/js/wysihtml5/bootstrap-wysihtml5.css">
 <script src="assets/js/wysihtml5/wysihtml5-0.4.0pre.min.js"></script>
 <script src="assets/js/wysihtml5/bootstrap-wysihtml5.js"></script>
