6e1e28d4b4962d1a35dc34fa5c1345066b65d533
umer <umer.zafar@code-desk.com>
2016-04-22 12:32:16 +0500

    Themes module url error fixed

--- app/controllers/ThemesController.php
+++ app/controllers/ThemesController.php
@@ -288,7 +288,7 @@ class ThemesController extends \BaseController {
             ///////////
 
             $rules = array(
-                'DomainUrl' => 'required|unique:tblCompanyThemes,DomainUrl,'.$id.',ThemeID|url',
+                'DomainUrl' => 'required|unique:tblCompanyThemes,DomainUrl,'.$id.',ThemeID',
                // 'FooterUrl' => 'url',
             );
 			
