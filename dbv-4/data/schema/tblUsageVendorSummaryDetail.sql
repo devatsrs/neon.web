a26fa77e69d30801d1e6fcbf2447caf4fa8bcca0
girish vadher <girish.vadher@code-desk.com>
2016-05-23 15:55:55 +0530

    Font Awesome 4.6.3 added

--- dbv-4/data/schema/tblUsageVendorSummaryDetail.sql
+++ dbv-4/data/schema/tblUsageVendorSummaryDetail.sql
@@ -13,5 +13,5 @@ CREATE TABLE `tblUsageVendorSummaryDetail` (
   KEY `FK_tblUsageVendorSummaryDetail_dim_time` (`TimeID`),
   KEY `FK_tblUsageVendorSummaryDetail_tblSummaryVendorHeader` (`SummaryVendorHeaderID`),
   CONSTRAINT `FK_tblUsageVendorSummaryDetail_tblDimTime` FOREIGN KEY (`TimeID`) REFERENCES `tblDimTime` (`TimeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_tblUsageVendorSummaryDetail_tblSummaryVendorHeader` FOREIGN KEY (`SummaryVendorHeaderID`) REFERENCES `tblUsageVendorSummary` (`SummaryVendorHeaderID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `FK_tblUsageVendorSummaryDetail_tblSummaryVendorHeader` FOREIGN KEY (`SummaryVendorHeaderID`) REFERENCES `tblSummaryVendorHeader` (`SummaryVendorHeaderID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
