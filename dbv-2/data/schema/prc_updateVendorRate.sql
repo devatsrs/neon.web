603c6a2bf1d250acb1f8cc5ff6f1674926372bd0
girish vadher <girish.vadher@code-desk.com>
2016-06-10 16:37:48 +0530

    Billed Duration when re rating

--- dbv-2/data/schema/prc_updateVendorRate.sql
+++ dbv-2/data/schema/prc_updateVendorRate.sql
@@ -11,18 +11,30 @@ BEGIN
 	UPDATE   LocalRMCdr.`' , p_tbltempusagedetail_name , '` ud 
 	INNER JOIN LocalRatemanagement.tmp_vcodes_ cr ON cr.Code = ud.area_prefix
 	SET selling_cost = 
-		CASE WHEN  billed_duration >= Interval1
+		CASE WHEN  duration >= Interval1
 		THEN
-			(Rate/60.0)*Interval1+CEILING((billed_duration-Interval1)/IntervalN)*(Rate/60.0)*IntervalN+IFNULL(ConnectionFee,0)
+			(Rate/60.0)*Interval1+CEILING((duration-Interval1)/IntervalN)*(Rate/60.0)*IntervalN+IFNULL(ConnectionFee,0)
 		ElSE
-			CASE WHEN  billed_duration > 0
+			CASE WHEN  duration > 0
 			THEN
 				Rate+IFNULL(ConnectionFee,0)
 			ELSE
 				0
 			END		    
 		END
-	,is_rerated=1
+	,is_rerated=1,
+	,billed_duration =
+		CASE WHEN  duration >= Interval1
+		THEN
+			Interval1+CEILING((duration-Interval1)/IntervalN)*IntervalN
+		ElSE 
+			CASE WHEN  duration > 0
+			THEN
+				Interval1
+			ELSE
+				0
+			END
+		END
 	WHERE ProcessID = "',p_processId,'"
 	AND AccountID = "',p_AccountID ,'" 
 	AND TrunkID = "',p_TrunkID ,'"') ;
