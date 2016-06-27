ed6e2f172f1426df514407729b54d7e6dff44438
deven <shriramsoft@gmail.com>
2016-06-27 12:44:37 +0530

    server monitor update

--- app/views/server/index.blade.php
+++ app/views/server/index.blade.php
@@ -1,22 +1,12 @@
-@extends('layout.main')
+@extends('layout.main_only_sidebar')
 @section('content')
-
-
-<ol class="breadcrumb bc-3">
-  <li> <a href="{{action('dashboard')}}"><i class="entypo-home"></i>Home</a> </li>
-  <li class="active"> <a href="javascript:void(0)">Server Information</a> </li>
-</ol>
-<h3>Server Information</h3>
-<div class="tab-content">
-  <div class="tab-pane active">
- <iframe width="100%;" height="1024px;" id="IframeServer" src="<?php echo "http://".$_SERVER['HTTP_HOST'].":19999/"; ?>"></iframe>
-   </div>
+ <iframe width="100%;" height="1024px;" id="IframeServer" src="<?php echo getenv("SERVER_MONITOR_URL"); ?>"></iframe>
   @include('includes.errors')
   @include('includes.success')
 <style>
 iframe{border:0px;}
+.main-content{ padding: 0 !important;}
 </style>
-   </div>
 @stop
 @section('footer_ext')
     @parent
