a8d6a2051629039f75a631a9b397465ff62aefe2
deven <shriramsoft@gmail.com>
2016-07-04 12:22:54 +0530

    user profile image fix

--- app/models/UserProfile.php
+++ app/models/UserProfile.php
@@ -12,8 +12,12 @@ class UserProfile extends \Eloquent {
         if(empty($user_profile_img)){
             $user_profile_img =  \Illuminate\Support\Facades\URL::to('assets/images/placeholder-male.gif');
         }else{
-            $user_profile_img = AmazonS3::unSignedImageUrl($user_profile_img);// str_replace("\\\\",'/',$destinationPath);
-            //$user_profile_img = URL::asset($destinationPath .'/' .$user_profile_img);
+
+            if(AmazonS3::getS3Client() == 'NoAmazon'){
+                $user_profile_img = \Illuminate\Support\Facades\URL::to($user_profile_img);
+            }else{
+                $user_profile_img = AmazonS3::unSignedImageUrl($user_profile_img);// str_replace("\\\\",'/',$destinationPath);
+            }
         }
         return $user_profile_img;
     }
