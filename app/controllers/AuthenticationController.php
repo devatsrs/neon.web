03a0b7ac3f799b458e707a93f4a4fb62b8971351
Abubakar <abubakar@code-desk.com>
2016-07-11 21:01:03 +0500

    Update Add Ip

--- app/controllers/AuthenticationController.php
+++ app/controllers/AuthenticationController.php
@@ -32,13 +32,13 @@ class AuthenticationController extends \BaseController
         if(isset($data['CustomerAuthValue'])){
             $data['CustomerAuthValue'] = implode(',', array_unique(explode(',', $data['CustomerAuthValue'])));
         }
-        if(!empty($data['VendorAuthRule']) && ($data['VendorAuthRule'] != 'IP' || $data['VendorAuthRule']!='Other')){
+        if(empty($data['VendorAuthRule']) || $data['VendorAuthRule'] != 'IP' || $data['VendorAuthRule']!='Other'){
             $data['VendorAuthValue']='';
 
         }else if(!empty($data['VendorAuthRule']) && $data['VendorAuthRule'] == 'Other' && empty($data['VendorAuthValue'])){
             return Response::json(array("status" => "error", "message" => "Vendor Other Value required"));
         }
-        if(!empty($data['CustomerAuthRule']) && ($data['CustomerAuthRule'] != 'IP' || $data['CustomerAuthRule']!='Other')){
+        if(empty($data['CustomerAuthRule']) || $data['CustomerAuthRule'] != 'IP' || $data['CustomerAuthRule']!='Other'){
             $data['CustomerAuthValue']='';
         }elseif(!empty($data['CustomerAuthRule']) && $data['CustomerAuthRule'] == 'Other' && empty($data['CustomerAuthValue'])){
             return Response::json(array("status" => "error", "message" => "Customer Other Value required"));
