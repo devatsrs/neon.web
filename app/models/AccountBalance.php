00d034381b35bf2d06b73f5c6813bc50e7337a46
girish vadher <girish.vadher@code-desk.com>
2016-07-12 11:40:46 +0530

    real time soa calculation

---
+++ app/models/AccountBalance.php
@@ -0,0 +1,43 @@
+<?php
+class AccountBalance extends \Eloquent {
+    //
+    protected $guarded = array("AccountBalanceID");
+
+    protected $table = 'tblAccountBalance';
+
+    protected $primaryKey = "AccountBalanceID";
+
+    public $timestamps = false; // no created_at and updated_at
+
+    public static function getUnbilledAmount($CompanyID,$AccountID){
+        $Amount = 0;
+        $LastInvoiceDate = Invoice::getLastInvoiceDate($CompanyID, $AccountID);
+        if(!empty($LastInvoiceDate)){
+            $UnbilledAmount = DB::connection('neon_report')->select("CALL prc_getUnbilledReport(?,?,?,?)",array($CompanyID,$AccountID,$LastInvoiceDate,3));
+            $Amount = $UnbilledAmount[0]->FinalAmount;
+        }
+        return $Amount;
+    }
+
+    public static function getAccountSOA($CompanyID,$AccountID){
+        $query = "call prc_getSOA (" . $CompanyID . "," . $AccountID . ",'','',0)";
+        $result = DataTableSql::of($query,'sqlsrv2')->getProcResult(array('InvoiceOut','PaymentIn','InvoiceIn','PaymentOut','InvoiceOutAmountTotal','InvoiceOutDisputeAmountTotal','PaymentInAmountTotal','InvoiceInAmountTotal','InvoiceInDisputeAmountTotal','PaymentOutAmountTotal'));
+
+        $InvoiceOutAmountTotal = $result['data']['InvoiceOutAmountTotal'];
+        $PaymentInAmountTotal = $result['data']['PaymentInAmountTotal'];
+        $InvoiceInAmountTotal = $result['data']['InvoiceInAmountTotal'];
+        $PaymentOutAmountTotal = $result['data']['PaymentOutAmountTotal'];
+
+        $InvoiceOutAmountTotal = ($InvoiceOutAmountTotal[0]->InvoiceOutAmountTotal > 0) ? $InvoiceOutAmountTotal[0]->InvoiceOutAmountTotal : 0;
+        $PaymentInAmountTotal = ($PaymentInAmountTotal[0]->PaymentInAmountTotal > 0) ? $PaymentInAmountTotal[0]->PaymentInAmountTotal : 0;
+        $InvoiceInAmountTotal = ($InvoiceInAmountTotal[0]->InvoiceInAmountTotal > 0) ? $InvoiceInAmountTotal[0]->InvoiceInAmountTotal : 0;
+        $PaymentOutAmountTotal = ($PaymentOutAmountTotal[0]->PaymentOutAmountTotal > 0) ? $PaymentOutAmountTotal[0]->PaymentOutAmountTotal : 0;
+
+        $OffsetBalance = ($InvoiceOutAmountTotal - $PaymentInAmountTotal) - ($InvoiceInAmountTotal - $PaymentOutAmountTotal);
+
+        return $OffsetBalance;
+
+
+    }
+
+}
