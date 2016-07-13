7aa3d484a7423fe10042165e4e00b191c464a42f
deven <shriramsoft@gmail.com>
2016-07-13 13:07:06 +0530

    Job Canceled

---
+++ dbv-1/data/schema/prc_UpdatePendingJobToCanceled.sql
@@ -0,0 +1,23 @@
+CREATE DEFINER=`neon-user`@`117.247.87.156` PROCEDURE `prc_UpdatePendingJobToCanceled`(IN `p_JobID` INT, IN `p_UserName` VARCHAR(500))
+BEGIN
+
+	SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+ 	
+ 	/*
+	 This procedure used to Cancel a job.
+	 */
+ 	
+ 	UPDATE tblJob 
+ 	SET 
+		JobStatusID = ( Select tblJobStatus.JobStatusID from tblJobStatus where tblJobStatus.Code = 'C' ),
+		ModifiedBy  = p_UserName,
+		JobStatusMessage = concat(  p_UserName , ' has canceled the job. ', '\n' , JobStatusMessage  ),
+		updated_at = now()
+ 	WHERE 
+ 	JobID  = p_JobID
+ 	AND JobStatusID = ( Select tblJobStatus.JobStatusID from tblJobStatus where tblJobStatus.Code = 'P' );
+	 
+	 
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+		
+END
