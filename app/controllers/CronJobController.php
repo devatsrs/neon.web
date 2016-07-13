2e69465895089b1a36b66826b5ae52d15d8f8ec0
deven <shriramsoft@gmail.com>
2016-07-13 17:48:03 +0530

    Log cron job history who disable enable , trigger , terminated

--- app/controllers/CronJobController.php
+++ app/controllers/CronJobController.php
@@ -290,6 +290,7 @@ class CronJobController extends \BaseController {
             $success = run_process($command);
         }
         if($success){
+            CronJobLog::createLog($CronJobID,["CronJobStatus"=>CronJob::CRON_SUCCESS, "Message" => "Triggered by " . User::get_user_full_name()]);
             return Response::json(array("status" => "success", "message" => "Cron Job is triggered." ));
         }else{
             return Response::json(array("status" => "failed", "message" => "Failed to trigger Cron Job"));
@@ -330,6 +331,7 @@ class CronJobController extends \BaseController {
         if(empty($CronJobID)){
             return Response::json(array("status" => "failed", "message" => "Invalid CronJobID." ));
         } else if(CronJob::find($CronJobID)->update(["Status"=>$Status])){
+            CronJobLog::createLog($CronJobID,["CronJobStatus"=>CronJob::CRON_SUCCESS, "Message" => $Status_to . " by " . User::get_user_full_name()]);
             return Response::json(array("status" => "success", "message" => $Status_to ));
         }else {
             return Response::json(array("status" => "failed", "message" => "Failed to Stop the Cron Job." ));
