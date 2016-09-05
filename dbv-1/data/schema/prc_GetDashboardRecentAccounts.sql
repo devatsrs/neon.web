ff240412b566b33daec84f7ddf4851e25ca380b1
umer <umer.zafar@code-desk.com>
2016-09-05 18:35:05 +0500

    crm dashboard recent accounts

--- dbv-1/data/schema/prc_GetDashboardRecentAccounts.sql
+++ dbv-1/data/schema/prc_GetDashboardRecentAccounts.sql
@@ -1,13 +1,15 @@
 CREATE DEFINER=`neon-user`@`104.47.140.143` PROCEDURE `prc_GetDashboardRecentAccounts`(
 	IN `p_companyId` int ,
-	IN `p_userId` int
+	IN `p_userId` VARCHAR(500),
+	IN `p_AccountManager` INT
+
 )
 BEGIN
 
     select  AccountID,AccountName,Phone,Email,created_by,created_at from tblAccount
           where
           `AccountType` = '1' and CompanyID = p_companyId and `Status` = '1'
-          AND (p_userId = '' OR  ( p_userId !='' AND find_in_set(Owner,p_userId)))
+          AND (  p_AccountManager = 0 OR (p_AccountManager = 1 AND find_in_set(tblAccount.Owner,p_userId)) )
     order by `tblAccount`.`AccountID` desc
 	 LIMIT 10;
 END
