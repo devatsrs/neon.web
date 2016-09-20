392d173fa7500f58caaab3eea1488457d9a05ed1
Abubakar <abubakar@code-desk.com>
2016-09-20 17:46:22 +0500

    update crm filter

--- app/views/dashboard/crm.blade.php
+++ app/views/dashboard/crm.blade.php
@@ -1,38 +1,39 @@
 @extends('layout.main')
 @section('content')
 
-<div class="row">
-  <div class="tab-content">
-    <div class="tab-pane active" id="customer" >
-      <div class="col-md-12">
-        <form novalidate class="form-horizontal form-groups-bordered filter validate" method="post" id="crm_dashboard">
-          <div data-collapsed="0" class="panel panel-primary">
-            <div class="panel-heading">
-              <div class="panel-title">
-                <h3> Filter</h3>
-              </div>
-              <div class="panel-options"> <a data-rel="collapse" href="#"><i class="entypo-down-open"></i></a> </div>
-            </div>
-            <div class="panel-body white-bg">
-              <div class="form-group"> @if(User::is_admin())
-                <label class="col-sm-1 control-label" for="field-1">User</label>
-                <div class="col-sm-6"> {{Form::select('UsersID[]', $users, '' ,array("class"=>"select2","multiple"=>"multiple"))}} </div>
-                @else
-                <input type="hidden" name="UsersID[]" value="{{User::get_userID()}}">
-                @endif
-                <label class="col-sm-1 control-label" for="field-1">Currency</label>
-                <div class="col-sm-2"> {{ Form::select('CurrencyID',$currency,$DefaultCurrencyID,array("class"=>"select2")) }} </div>
-              </div>
-              <p style="text-align: right;">
-                <button class="btn btn-primary btn-sm btn-icon icon-left" type="submit"> <i class="entypo-search"></i> Search </button>
-              </p>
-            </div>
-          </div>
-        </form>
-      </div>
+    <div class="row">
+        <div id="customer" class="col-sm-12">
+            <form novalidate="novalidate" class="form-horizontal form-groups-bordered validate" method="post" id="crm_dashboard">
+                <div data-collapsed="0" class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            Filter
+                        </div>
+                        <div class="panel-options">
+                            <a data-rel="collapse" href="#">
+                                <i class="entypo-down-open"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="form-group"> @if(User::is_admin())
+                                <label class="col-sm-1 control-label" for="field-1">User</label>
+                                <div class="col-sm-6"> {{Form::select('UsersID[]', $users, '' ,array("class"=>"select2","multiple"=>"multiple"))}} </div>
+                            @else
+                                <input type="hidden" name="UsersID[]" value="{{User::get_userID()}}">
+                            @endif
+                            <label class="col-sm-1 control-label" for="field-1">Currency</label>
+                            <div class="col-sm-2"> {{ Form::select('CurrencyID',$currency,$DefaultCurrencyID,array("class"=>"select2")) }} </div>
+                        </div>
+                        <p style="text-align: right;">
+                            <button class="btn btn-primary btn-sm btn-icon icon-left" type="submit"> <i class="entypo-search"></i> Search </button>
+                        </p>
+                    </div>
+                </div>
+            </form>
+        </div>
+
     </div>
-  </div>
-</div>
 <?php if(User::checkCategoryPermission('CrmDashboardTasks','View')){?>
 <div class="row">
   <div class="col-sm-12">
@@ -405,6 +406,10 @@ var RevenueReport = 0;
 	.panel-heading{
 	border:none !important;
 	}
+    #customer .panel-heading{
+        border-bottom:1px solid transparent !important;
+        border-color:#ebebeb !important;
+    }
 </style>
 @stop
 @section('footer_ext')
