e47261e3e6f09cd61807df22a661be252fe50882
umer <umer.zafar@code-desk.com>
2016-09-30 17:14:30 +0500

    Email tracking

--- app/views/includes/dashboard-top.blade.php
+++ app/views/includes/dashboard-top.blade.php
@@ -227,7 +227,7 @@
          <div class="modal-content">
              <div class="modal-header">
                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                 <h4 class="modal-title">Message Content</h4>
+                 <h4 class="modal-title">Job Content</h4>
              </div>
              <div class="modal-body">
                  Content is loading...
@@ -240,7 +240,7 @@
          <div class="modal-content">
              <div class="modal-header">
                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                 <h4 class="modal-title">Job Content</h4>
+                 <h4 class="modal-title">Message Content</h4>
              </div>
              <div class="modal-body">
                  Content is loading...
