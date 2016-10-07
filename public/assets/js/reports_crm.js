7966dc037e6f4a62fa8f0eb37c64a42eea0e838a
umer <umer.zafar@code-desk.com>
2016-10-07 17:24:33 +0500

    Crm dashboard permission in tblcompayconfiguration

--- public/assets/js/reports_crm.js
+++ public/assets/js/reports_crm.js
@@ -152,7 +152,7 @@
                                 action += '<input type = "hidden"  name = "' + opportunity[i] + '" value = "' + (full[i] != null?full[i]:'')+ '" / >';
                             }
                             action += '</div>';
-                           if(task_edit==1){
+                           if(Opportunity_edit==1){
                             action += ' <button type="button" data-id="' + full[2] + '" class="edit-deal btn btn-default"><i class="entypo-pencil"></i></button>';
 							}
 							$('.loaderopportunites').hide();
@@ -180,6 +180,8 @@
 		}
 			//opportunites grid end
 			
+			function GetUsersTasks(){ 
+			if(CrmDashboardTasks===0){return false;} 
 			///task grid start
 			 data_table1 = $("#taskGrid").dataTable({
                 "bDestroy": true,
@@ -270,6 +272,7 @@
 
             });
 			///task grid end
+			}
 			
             Highcharts.theme = {
                 colors: ['#3366cc', '#ff9900' ,'#dc3912' , '#109618', '#66aa00', '#dd4477','#0099c6', '#990099', '#143DFF']
@@ -429,7 +432,8 @@ function reloadCrmCharts(){
 $(function() {   
 	 
 	  $("#DueDateFilter").change(function(){
-       data_table1.fnFilter('',0);
+       //data_table1.fnFilter('',0); 
+	   GetUsersTasks();
     });
 	
 	$('#crm_dashboard_Sales').submit(function(e) {
@@ -469,8 +473,8 @@ $('body').on('click', '.panel > .panel-heading > .panel-options > a[data-rel="re
     var id = $(this).parents().attr('id');
 	var submit_form = $("#crm_dashboard");
     if(id=='UsersTasks'){
-      //  GetUsersTasks();
-	  data_table1.fnFilter('',0);
+        GetUsersTasks();
+	 // data_table1.fnFilter('',0);
 	  return false;
     }
 	if(id=='Pipeline'){
@@ -869,10 +873,7 @@ function GetSalesData(){
     });
 	}
 
-function GetUsersTasks(){	
-	 if(CrmDashboardTasks===0){return false;} 
-	data_table1.fnFilter('',0);
-}
+
 
 function GetAccounts(){
 	////////////////////
@@ -920,7 +921,7 @@ function GetAccounts(){
 
 //opportunity edit start
             $(document).on('click','#board-start ul.sortable-list li button.edit-deal,#opportunityGrid .edit-deal',function(e){
-				 if(Opportunity_edit==1){
+				 if(CrmDashboardOpportunities==1){
 	            e.stopPropagation();
                 if($(this).is('button')){
                     var rowHidden = $(this).prev('div.hiddenRowData');
@@ -1052,7 +1053,7 @@ function GetAccounts(){
                     } else {
                         toastr.error(response.message, "Error", toastr_opts);
                     }
-					data_table.fnFilter('',0);
+					Getopportunities();
                     $("#opportunity-update").button('reset');                   
                   
                 },
@@ -1090,9 +1091,9 @@ function GetAccounts(){
                     }else{
                         toastr.error(response.message, "Error", toastr_opts);
                     }
-					data_table1.fnFilter('',0);
+					//data_table1.fnFilter('',0);
+					GetUsersTasks();
                     $("#task-update").button('reset');     
-                    //getOpportunities();
                 },
                 // Form data
                 data: formData,
