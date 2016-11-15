f9b7df5f983a3b3f6d7aa8bf58a7d9a6e187162b
girish vadher <girish.vadher@code-desk.com>
2016-11-15 13:11:16 +0530

    Notification  updates

--- dbv-4/data/schema/prc_getACD_ASR_Alert.sql
+++ dbv-4/data/schema/prc_getACD_ASR_Alert.sql
@@ -1,4 +1,14 @@
-CREATE DEFINER=`neon-user`@`117.247.87.156` PROCEDURE `prc_getACD_ASR_Alert`(IN `p_CompanyID` INT, IN `p_CompanyGatewayID` TEXT, IN `p_AccountID` TEXT, IN `p_CurrencyID` INT, IN `p_StartDate` DATETIME, IN `p_EndDate` DATETIME, IN `p_AreaPrefix` TEXT, IN `p_Trunk` TEXT, IN `p_CountryID` TEXT)
+CREATE DEFINER=`neon-user`@`117.247.87.156` PROCEDURE `prc_getACD_ASR_Alert`(
+	IN `p_CompanyID` INT,
+	IN `p_CompanyGatewayID` TEXT,
+	IN `p_AccountID` TEXT,
+	IN `p_CurrencyID` INT,
+	IN `p_StartDate` DATETIME,
+	IN `p_EndDate` DATETIME,
+	IN `p_AreaPrefix` TEXT,
+	IN `p_Trunk` TEXT,
+	IN `p_CountryID` TEXT
+)
 BEGIN
 	
 	DECLARE v_Round_ INT;
@@ -13,7 +23,10 @@ BEGIN
 	THEN
 		SELECT
 			IF(SUM(NoOfCalls)>0,fnDurationmmss(COALESCE(SUM(TotalBilledDuration),0)/SUM(NoOfCalls)),0) as ACD , 
-			ROUND(SUM(NoOfCalls)/(SUM(NoOfCalls)+SUM(NoOfFailCalls))*100,v_Round_) as ASR
+			ROUND(SUM(NoOfCalls)/(SUM(NoOfCalls)+SUM(NoOfFailCalls))*100,v_Round_) as ASR,
+			HOUR(ANY_VALUE(Time)) as Hour,
+			COALESCE(SUM(NoOfCalls),0) as Connected,
+			COALESCE(SUM(NoOfCalls),0)+COALESCE(SUM(NoOfFailCalls),0) as Attempts
 		FROM tmp_tblUsageSummary_ us;
 		
 	END IF;
@@ -23,7 +36,10 @@ BEGIN
 		SELECT
 			IF(SUM(NoOfCalls)>0,fnDurationmmss(COALESCE(SUM(TotalBilledDuration),0)/SUM(NoOfCalls)),0) as ACD , 
 			ROUND(SUM(NoOfCalls)/(SUM(NoOfCalls)+SUM(NoOfFailCalls))*100,v_Round_) as ASR,
-			AccountID
+			AccountID,
+			HOUR(ANY_VALUE(Time)) as Hour,
+			COALESCE(SUM(NoOfCalls),0) as Connected,
+			COALESCE(SUM(NoOfCalls),0)+COALESCE(SUM(NoOfFailCalls),0) as Attempts
 		FROM tmp_tblUsageSummary_ us
 		GROUP BY AccountID;
 	END IF;
