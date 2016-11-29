e1aaf426b8bdcafec7c828594ec602c4779519c3
girish vadher <girish.vadher@code-desk.com>
2016-11-29 16:04:58 +0530

    analysis page style changed

--- app/views/vendoranalysis/index.blade.php
+++ app/views/vendoranalysis/index.blade.php
@@ -28,14 +28,14 @@
                             <div class="form-group">
                                 <label class="col-sm-1 control-label" for="field-1">Start Date</label>
                                 <div class="col-sm-2" style="padding-left:0; padding-right:0; width:10%;">
-                                    <input type="text" name="StartDate"  class="form-control datepicker"  data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}" data-enddate="{{date('Y-m-d')}}"/>
+                                    <input type="text" name="StartDate"  class="form-control datepicker small_fld"  data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}" data-enddate="{{date('Y-m-d')}}"/>
                                 </div>
                                 <div class="col-md-1 select_hour" style="padding: 0px; width: 9%;">
                                     <input type="text" name="StartHour" data-minute-step="30"   data-show-meridian="false" data-default-time="00:00" value="00:00"  data-template="dropdown" class="form-control timepicker small_fld">
                                 </div>
                                 <label class="col-sm-1 control-label" for="field-1">End Date</label>
                                 <div class="col-sm-2" style="padding-left:0; padding-right:0; width:10%;">
-                                    <input type="text" name="EndDate" class="form-control datepicker"  data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}" data-enddate="{{date('Y-m-d' )}}" />
+                                    <input type="text" name="EndDate" class="form-control datepicker small_fld"  data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}" data-enddate="{{date('Y-m-d' )}}" />
                                 </div>
                                 <div class="col-md-1 select_hour" style="padding: 0px; width: 9%;">
                                     <input type="text" name="EndHour" data-minute-step="30"   data-show-meridian="false" data-default-time="23:30" value="23:30"   data-template="dropdown" class="form-control timepicker small_fld">
