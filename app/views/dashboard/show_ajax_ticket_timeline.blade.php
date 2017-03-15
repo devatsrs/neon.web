864f09f34c909b0299666d2bb428ec3fdb4b616a
Abubakar <abubakar@code-desk.com>
2017-03-15 19:15:47 +0500

    Update ticket dashboard

--- app/views/dashboard/show_ajax_ticket_timeline.blade.php
+++ app/views/dashboard/show_ajax_ticket_timeline.blade.php
@@ -1,16 +1,26 @@
  @if(count($response)>0)
      @foreach($response as $row)
          <?php
-                 $text = $row->TimeLineType==1?'email response':'add note';
-                 $action = $row->TimeLineType==1?($row->EmailParent==0?'Submitted a new ticket':'Reply to'):'add note';
-                 $target = ($row->TimeLineType==1?'#message':'#note').$row->ID
+         $target = ($row->TimeLineType==1?'#message':'#note').$row->ID;
+         $text = $row->TimeLineType==1?($row->EmailParent==0?'Submitted a new ticket':'Reply'):'add note';
          ?>
      <li>
          <div class="thumb">{{ucfirst($row->UserName[0])}}</div>
          <div class="description">
-             <a href="{{ $row->TimeLineType==1? URL::to('accounts/'.$row->UserID.'/show'):'#'}}" target="_blank" class="username">{{ucfirst($row->UserName)}}</a>
-             <span>{{$action}}</span>
-             @if($row->EmailParent > 0)
+             @if($row->TimeLineType==1)
+                <a href="{{URL::to('accounts/'.$row->UserID.'/show')}}" target="_blank" class="username">{{ucfirst($row->UserName)}}</a>
+             @else
+                 <span>{{ucfirst($row->UserName)}}</span>
+             @endif
+
+             @if($row->TimeLineType == 1)
+                @if($row->EmailParent=0)
+                     <span>{{$text}}</span>
+                    @else
+                     <a href="{{URL::to('/tickets/'.$row->TicketID.'/detail').$target}}">{{$text}}</a>
+                     <span>to the ticket</span>
+                 @endif
+             @else
                  <a href="{{URL::to('/tickets/'.$row->TicketID.'/detail').$target}}">{{$text}}</a>
                  <span>to the ticket</span>
              @endif
