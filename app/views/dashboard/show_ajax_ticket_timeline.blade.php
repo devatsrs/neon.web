b6f184f04f9e85e4cb323757e89a1fad2cbc65c7
Abubakar <abubakar@code-desk.com>
2017-03-15 18:09:48 +0500

    Update ticket dashboard

--- app/views/dashboard/show_ajax_ticket_timeline.blade.php
+++ app/views/dashboard/show_ajax_ticket_timeline.blade.php
@@ -2,13 +2,14 @@
      @foreach($response as $row)
          <?php
                  $text = $row->TimeLineType==1?'email response':'add note';
+                 $action = $row->TimeLineType==1?($row->EmailParent==0?'Submitted a new ticket':'Reply to'):'add note';
                  $target = ($row->TimeLineType==1?'#message':'#note').$row->ID
          ?>
      <li>
          <div class="thumb">{{ucfirst($row->UserName[0])}}</div>
          <div class="description">
              <a href="{{ $row->TimeLineType==1? URL::to('accounts/'.$row->UserID.'/show'):'#'}}" target="_blank" class="username">{{ucfirst($row->UserName)}}</a>
-             <span>{{$row->EmailParent==0?'Submitted a new ticket':'sent an'}}</span>
+             <span>{{$action}}</span>
              @if($row->EmailParent > 0)
                  <a href="{{URL::to('/tickets/'.$row->TicketID.'/detail').$target}}">{{$text}}</a>
                  <span>to the ticket</span>
