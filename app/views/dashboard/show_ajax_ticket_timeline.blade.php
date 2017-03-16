6fb418272d464a442d5318b30ac5997ca22d1fc2
Abubakar <abubakar@code-desk.com>
2017-03-16 16:37:10 +0500

    Update ticketDashbaord

--- app/views/dashboard/show_ajax_ticket_timeline.blade.php
+++ app/views/dashboard/show_ajax_ticket_timeline.blade.php
@@ -1,19 +1,19 @@
  @if(count($response)>0)
      @foreach($response as $row)
          <?php
-         $target = ($row->TimeLineType==1?'#message':'#note').$row->ID;
-         $text = $row->TimeLineType==1?($row->EmailParent==0?'Submitted a new ticket':'Reply'):'add note';
+         $target = ($row->TimelineType==1?'#message':'#note').$row->RecordID;
+         $text = $row->TimelineType==1?($row->EmailParent==0?'Submitted a new ticket':'Reply'):'add note';
          ?>
      <li>
          <div class="thumb">{{ucfirst($row->UserName[0])}}</div>
          <div class="description">
-             @if($row->TimeLineType==1)
+             @if($row->TimelineType==1)
                 <a href="{{URL::to('accounts/'.$row->UserID.'/show')}}" target="_blank" class="username">{{ucfirst($row->UserName)}}</a>
              @else
                  <span>{{ucfirst($row->UserName)}}</span>
              @endif
 
-             @if($row->TimeLineType == 1)
+             @if($row->TimelineType == 1)
                 @if($row->EmailParent=0)
                      <span>{{$text}}</span>
                     @else
