78f5bfb12abd064b6463955f193fbc89e6983568
Abubakar <abubakar@code-desk.com>
2017-03-24 14:35:22 +0500

    Update ticket dashboard

--- app/views/dashboard/ticket.blade.php
+++ app/views/dashboard/ticket.blade.php
@@ -87,7 +87,7 @@
     <?php } ?>
     @if(User::checkCategoryPermission('TicketDashboardTimeLineWidgets','View'))
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div data-collapsed="0" class="panel panel-primary">
                 <div class="panel-heading">
                     <div class="panel-title">
