0f0d2afd100d7d4be3fdb39f1e7000efd6d1679c
Abubakar <abubakar@code-desk.com>
2017-03-28 20:59:06 +0500

    Add DB script

--- dbv-1/data/revisions/4.10/abubakar.sql
+++ dbv-1/data/revisions/4.10/abubakar.sql
@@ -101,3 +101,270 @@ BEGIN
     END IF;
 	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 END
+
+DROP TABLE IF EXISTS `tblTicketLog`;
+CREATE TABLE IF NOT EXISTS `tblTicketLog` (
+  `TicketLogID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserID` int(11) DEFAULT '0',
+  `AccountID` int(11) DEFAULT '0',
+  `CompanyID` int(11) DEFAULT NULL,
+  `TicketID` int(11) DEFAULT NULL,
+  `TicketFieldID` int(11) DEFAULT '0',
+  `TicketFieldValueFromID` int(11) DEFAULT '0',
+  `TicketFieldValueToID` int(11) DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`TicketLogID`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `tblTicketDashboardTimeline`;
+CREATE TABLE IF NOT EXISTS `tblTicketDashboardTimeline` (
+  `TicketDashboardTimelineID` int(11) NOT NULL AUTO_INCREMENT,
+  `CompanyID` int(11) NOT NULL DEFAULT '0',
+  `TimelineType` int(11) DEFAULT NULL,
+  `UserName` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `CustomerID` int(11) DEFAULT NULL,
+  `EmailCall` int(11) DEFAULT NULL,
+  `TicketID` int(11) DEFAULT NULL,
+  `TicketSubmit` int(11) DEFAULT NULL,
+  `Subject` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `RecordID` int(11) DEFAULT NULL,
+  `AgentID` int(11) DEFAULT NULL,
+  `GroupID` int(11) DEFAULT NULL,
+  `TicketFieldID` int(11) DEFAULT NULL,
+  `TicketFieldValueFromID` int(11) DEFAULT NULL,
+  `TicketFieldValueToID` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_at_table` datetime DEFAULT NULL,
+  PRIMARY KEY (`TicketDashboardTimelineID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+DROP PROCEDURE IF EXISTS `prc_GetTicketDashboardTimeline`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_GetTicketDashboardTimeline`(
+	IN `p_CompanyID` INT,
+	IN `P_Group` INT
+,
+	IN `P_Agent` INT
+,
+	IN `p_Time` DATETIME,
+	IN `p_PageNumber` INT,
+	IN `p_RowsPage` INT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+BEGIN
+	DECLARE v_MAXDATE DATETIME;
+
+	SELECT MAX(created_at) as created_at INTO v_MAXDATE FROM tblTicketDashboardTimeline;
+	IF(p_PageNumber=0)
+	THEN
+		DELETE FROM tblTicketDashboardTimeline WHERE created_at_table < DATE_SUB(p_Time, INTERVAL 1 MONTH);
+		INSERT INTO tblTicketDashboardTimeline
+		SELECT
+			NULL,
+			tc.CompanyID,
+			1 as TimeLineType,
+			CONCAT(IFNULL(u.FirstName,''),' ',IFNULL(u.LastName,'')) as UserName,
+			u.UserID,
+			0 as CustomerID,
+			acel.EmailCall,
+			tc.TicketID,
+			IF(tc.AccountEmailLogID = 0,0,1) as TicketSubmit,
+			tc.Subject,
+			acel.AccountEmailLogID as ID,
+			tc.Agent,
+			tc.`Group`,
+			0 as TicketFieldID,
+			0 as TicketFieldValueFromID,
+			0 as TicketFieldValueToID,
+			acel.created_at,
+			p_Time
+		FROM AccountEmailLog acel
+		INNER JOIN tblTickets tc ON tc.TicketID = acel.TicketID
+		LEFT JOIN tblAccount ac ON ac.AccountID = acel.AccountID
+		LEFT JOIN tblUser u ON u.UserID = acel.UserID
+		WHERE acel.CompanyID = p_CompanyID
+		AND (v_MAXDATE IS NULL OR acel.created_at > v_MAXDATE)
+		AND acel.EmailCall = 0
+		AND u.UserID IS NOT NULL;
+
+		INSERT INTO tblTicketDashboardTimeline
+		SELECT
+		NULL,
+		tc.CompanyID,
+		2 as TimeLineType,
+		CONCAT(IFNULL(u.FirstName,''),' ',IFNULL(u.LastName,''))  as UserName,
+		TN.UserID as UserID,
+		0 as CustomerID,
+		0 as EmailCall,
+		tc.TicketID,
+		IF(tc.AccountEmailLogID = 0,0,1) as TicketSubmit,
+		tc.Subject,
+		TN.NoteID as ID,
+		tc.Agent,
+		tc.`Group`,
+		0 as TicketFieldID,
+		0 as TicketFieldValueFromID,
+		0 as TicketFieldValueToID,
+		TN.created_at,
+		p_Time
+		FROM `tblNote` TN
+		INNER JOIN tblTickets tc ON tc.TicketID = TN.TicketID
+		INNER JOIN tblUser u ON u.UserID = TN.UserID
+		WHERE TN.CompanyID = p_CompanyID
+		AND (v_MAXDATE IS NULL OR TN.created_at > v_MAXDATE);
+
+		INSERT INTO tblTicketDashboardTimeline
+		SELECT
+			NULL,
+			tc.CompanyID,
+			3 as TimeLineType,
+			CASE WHEN tl.AccountID = 0 THEN CONCAT(IFNULL(u.FirstName,''),' ',IFNULL(u.LastName,'')) ELSE CONCAT(IFNULL(a.FirstName,''),' ',IFNULL(a.LastName,''))  END as UserName,
+			tl.UserID as UserID,
+			tl.AccountID,
+			0 as EmailCall,
+			tc.TicketID,
+			IF(tc.AccountEmailLogID = 0,0,1) as TicketSubmit,
+			tc.Subject,
+			tl.TicketLogID as ID,
+			tc.Agent,
+			tc.`Group`,
+			tl.TicketFieldID,
+			tl.TicketFieldValueFromID,
+			tl.TicketFieldValueToID,
+			tl.created_at,
+			p_Time
+		FROM tblTicketLog tl
+		INNER JOIN tblTickets tc ON tc.TicketID = tl.TicketID
+		LEFT JOIN tblUser u ON u.UserID = tl.UserID
+		LEFT JOIN tblAccount a ON a.AccountID = tl.AccountID
+		WHERE tl.CompanyID = p_CompanyID
+		AND (v_MAXDATE IS NULL OR tl.created_at > v_MAXDATE);
+	END IF;
+	SELECT * FROM tblTicketDashboardTimeline tl
+	WHERE (P_Agent = 0 OR tl.AgentID = p_Agent)
+	AND(P_Group = 0 OR tl.`GroupID` = p_Group)
+	ORDER BY created_at DESC
+	LIMIT p_RowsPage OFFSET p_PageNumber;
+END//
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS `prc_GetCronJobHistory`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_GetCronJobHistory`(
+	IN `p_CronJobID` INT,
+	IN `p_StartDate` DATETIME,
+	IN `p_EndDate` DATETIME,
+	IN `p_SearchText` VARCHAR(50),
+	IN `p_Status` VARCHAR(50),
+	IN `p_PageNumber` INT,
+	IN `p_RowspPage` INT,
+	IN `p_lSortCol` VARCHAR(50),
+	IN `p_SortOrder` VARCHAR(5),
+	IN `p_isExport` INT
+)
+BEGIN
+
+   DECLARE v_OffSet_ int;
+   SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+
+	SET v_OffSet_ = (p_PageNumber * p_RowspPage) - p_RowspPage;
+
+    IF p_isExport = 0
+    THEN
+            SELECT
+                tblCronJobCommand.Title,
+                tblCronJobLog.CronJobStatus,
+                tblCronJobLog.Message,
+                tblCronJobLog.created_at
+            FROM tblCronJob
+            INNER JOIN tblCronJobLog
+                ON tblCronJob.CronJobID = tblCronJobLog.CronJobID
+                AND tblCronJobLog.CronJobID = p_CronJobID
+                AND (p_Status = '' OR tblCronJobLog.CronJobStatus = p_Status)
+                AND tblCronJobLog.created_at between p_StartDate and p_EndDate
+                AND (p_SearchText='' OR tblCronJobLog.Message like Concat('%',p_SearchText,'%'))
+            INNER JOIN tblCronJobCommand
+                ON tblCronJobCommand.CronJobCommandID = tblCronJob.CronJobCommandID
+            ORDER BY
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'TitleDESC') THEN tblCronJobCommand.Title
+                END DESC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'TitleASC') THEN tblCronJobCommand.Title
+                END ASC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'CronJobStatusDESC') THEN tblCronJobLog.CronJobStatus
+                END DESC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'CronJobStatusASC') THEN tblCronJobLog.CronJobStatus
+                END ASC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'MessageDESC') THEN tblCronJobLog.Message
+                END DESC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'MessageASC') THEN tblCronJobLog.Message
+                END ASC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atDESC') THEN tblCronJobLog.created_at
+                END DESC,
+                CASE
+                    WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atASC') THEN tblCronJobLog.created_at
+                END ASC
+           LIMIT p_RowspPage OFFSET v_OffSet_;
+
+        SELECT
+            COUNT(tblCronJobLog.CronJobID) as totalcount
+        FROM tblCronJob
+            INNER JOIN tblCronJobLog
+                ON tblCronJob.CronJobID = tblCronJobLog.CronJobID
+                AND tblCronJobLog.CronJobID = p_CronJobID
+                AND (p_Status = '' OR tblCronJobLog.CronJobStatus = p_Status)
+                AND tblCronJobLog.created_at between p_StartDate and p_EndDate
+                AND (p_SearchText='' OR tblCronJobLog.Message like Concat('%',p_SearchText,'%'))
+            INNER JOIN tblCronJobCommand
+                ON tblCronJobCommand.CronJobCommandID = tblCronJob.CronJobCommandID;
+    END IF;
+
+    IF p_isExport = 1
+    THEN
+        SELECT
+            tblCronJobCommand.Title,
+            tblCronJobLog.CronJobStatus,
+            tblCronJobLog.Message,
+            tblCronJobLog.created_at
+        FROM tblCronJob
+        INNER JOIN tblCronJobLog
+                ON tblCronJob.CronJobID = tblCronJobLog.CronJobID
+                AND tblCronJobLog.CronJobID = p_CronJobID
+                AND (p_Status = '' OR tblCronJobLog.CronJobStatus = p_Status)
+                AND tblCronJobLog.created_at between p_StartDate and p_EndDate
+                AND (p_SearchText='' OR tblCronJobLog.Message like Concat('%',p_SearchText,'%'))
+        INNER JOIN tblCronJobCommand
+            ON tblCronJobCommand.CronJobCommandID = tblCronJob.CronJobCommandID;
+    END IF;
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END//
+DELIMITER ;
+
+
