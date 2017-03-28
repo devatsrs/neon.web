06d3f26479637814c9bb85405495698b4a02220b
Abubakar <abubakar@code-desk.com>
2017-03-28 20:02:35 +0500

    Update ticket dashbaord

--- app/views/dashboard/ticket.blade.php
+++ app/views/dashboard/ticket.blade.php
@@ -114,7 +114,7 @@
             SummaryWidgets(1);
             last_msg_funtion();
             $(window).scroll(function(){
-                if ($(window).scrollTop() == $(document).height() - $(window).height()){
+                if ($(window).scrollTop() >= ($('#activity-timeline li:last-child').offset().top - 300 )){
 
                     setTimeout(function() {
                         last_msg_funtion(0);
