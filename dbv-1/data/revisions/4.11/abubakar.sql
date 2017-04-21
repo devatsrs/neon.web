19626992856e23682bce435f279788e6b050ceba
Abubakar <abubakar@code-desk.com>
2017-04-21 19:53:28 +0500

    Add due and Overdue in ticket dashbaord

--- dbv-1/data/revisions/4.11/abubakar.sql
+++ dbv-1/data/revisions/4.11/abubakar.sql
@@ -355,6 +355,82 @@ BEGIN
 END//
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS `prc_CheckDueTickets`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_CheckDueTickets`(
+	IN `p_CompanyID` int,
+	IN `p_currentDateTime` DATETIME,
+	IN `P_Group` VARCHAR(50),
+	IN `P_Agent` VARCHAR(50)
+)
+BEGIN
+	DECLARE P_Status varchar(100);
+
+	SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	SET  sql_mode='';
+
+	SELECT
+		 group_concat(TFV.ValuesID separator ',') INTO P_Status FROM tblTicketfieldsValues TFV
+	LEFT JOIN tblTicketfields TF
+		ON TF.TicketFieldsID = TFV.FieldsID
+	WHERE
+		TF.FieldType = 'default_status' AND TFV.FieldValueAgent!='Closed' AND TFV.FieldValueAgent!='Resolved';
+
+	 DROP TEMPORARY TABLE IF EXISTS tmp_tickets_sla_voilation_;
+	CREATE TEMPORARY TABLE IF NOT EXISTS tmp_tickets_sla_voilation_(
+		TicketID int,
+		TicketSlaID int,
+		CreatedDate datetime,
+		ResolveTime datetime,
+		DueDate datetime,
+		IsResolvedVoilation int
+	);
+		insert into tmp_tickets_sla_voilation_
+		SELECT
+			T.TicketID,
+			T.TicketSlaID as TicketSlaID,
+			T.created_at as CreatedDate,
+	  	CASE WHEN (TST.ResolveType = 'Minute') THEN
+		       DATE_ADD(T.DueDate, INTERVAL TST.ResolveValue Minute)
+	 	  	  WHEN ResolveType = 'Hour' THEN
+		   	 DATE_ADD(T.DueDate, INTERVAL TST.ResolveValue Hour)
+		 	  WHEN (TST.ResolveType = 'Day') THEN
+		       DATE_ADD(T.DueDate, INTERVAL TST.ResolveValue Day)
+	 	  	  WHEN ResolveType = 'Month' THEN
+	   		 DATE_ADD(T.DueDate, INTERVAL TST.ResolveValue Month)
+	  END AS ResolveTime,
+	  T.DueDate,
+	  T.ResolveSlaPolicyVoilationEmailStatus AS IsResolvedVoilation
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketSlaTarget TST
+			ON TST.TicketSlaID = T.TicketSlaID
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND TST.PriorityID = T.Priority
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (T.RespondSlaPolicyVoilationEmailStatus = 0 OR T.ResolveSlaPolicyVoilationEmailStatus = 0)
+			AND T.TicketSlaID>0
+			AND (P_Group = '' OR FIND_IN_SET(T.`Group`,P_Group))
+			AND (P_Agent = '' OR FIND_IN_SET(T.`Agent`,P_Agent));
+
+
+			UPDATE tmp_tickets_sla_voilation_ TSV SET
+			TSV.IsResolvedVoilation  =
+			CASE
+				WHEN p_currentDateTime>=TSV.ResolveTime THEN 1 ELSE 0
+			END;
+
+			/*SELECT * FROM tmp_tickets_sla_voilation_ where IsResolvedVoilation >0;*/
+			select count(*) as TotalOverDue from tmp_tickets_sla_voilation_ where IsResolvedVoilation >0;
+			select count(*) as TotalDueToday from tmp_tickets_sla_voilation_ where IsResolvedVoilation >0 and DATE(p_currentDateTime) = DATE(ResolveTime);
+
+
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END//
+DELIMITER ;
+
+
 
 
 
