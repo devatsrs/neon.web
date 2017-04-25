2f2f4aa50962936120b3097b82a730ed63fe4056
deven <shriramsoft@gmail.com>
2017-04-25 17:37:36 +0530

    SLA  .sql

--- dbv-1/data/revisions/4.11/dev.sql
+++ dbv-1/data/revisions/4.11/dev.sql
@@ -5,3 +5,10 @@ UPDATE tblEmailTemplate SET  TemplateBody = REPLACE(TemplateBody, '<!--[if !mso]
 
 ALTER TABLE `tblTickets`
 	ADD COLUMN `TicketSlaID` INT NULL AFTER `EscalationEmail`;
+	ADD COLUMN `DueDate` INT NULL AFTER `TicketSlaID`;
+	ADD COLUMN `TicketSlaID` INT NULL AFTER `DueDate`;
+
+
+CREATE PROCEDURE `prc_AssignSlaToTicket`;
+
+CREATE PROCEDURE `prc_UpdateTicketSLADueDate`;
