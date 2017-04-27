69bd198894a638a835e93028b916ea1814cc3f66
deven <shriramsoft@gmail.com>
2017-04-27 20:02:14 +0530

    due time in detail

--- app/models/TicketLog.php
+++ app/models/TicketLog.php
@@ -1,6 +1,5 @@
 <?php
-namespace Api\Model;
-
+ 
 class TicketLog extends \Eloquent
 {
     protected $guarded = array("TicketLogID");
@@ -19,21 +18,5 @@ class TicketLog extends \Eloquent
     ];
 
 
-    public static function AddLog($TicketID,$isCustomer = 0){
-        if($isCustomer == 1){
-            $UserID = 0;
-            $AccountID = User::get_userID();
-        }else {
-            $UserID = User::get_userID();
-            $AccountID = 0;
-        }
-        $CompanyID = User::get_companyID();
-        $data = ['UserID' => $UserID,
-            'AccountID' => $AccountID,
-            'CompanyID' => $CompanyID,
-            'TicketID' => $TicketID,
-            'created_at' => date("Y-m-d H:i:s")];
-        TicketLog::insert($data);
-    }
 
 }
