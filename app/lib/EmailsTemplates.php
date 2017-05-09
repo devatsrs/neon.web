151d421c05a04a456efe5ddad9de46190763c776
bhavin machhoya <bhavin@code-desk.com>
2017-05-09 16:09:25 +0530

    Invoice Paid issues

--- app/lib/EmailsTemplates.php
+++ app/lib/EmailsTemplates.php
@@ -347,10 +347,10 @@ class EmailsTemplates{
 			$array['OutstandingExcludeUnbilledAmount'] = Account::getOutstandingAmount($companyID, $AccountID, get_round_decimal_places($AccountID));
 			$array['OutstandingIncludeUnbilledAmount'] = AccountBalance::getBalanceAmount($AccountID);
 			$array['BalanceThreshold'] 				   = AccountBalance::getBalanceThresholdAmount($AccountID);
-			$array['User'] 							   = User::get_user_full_name();
 			$UserID = user::get_userID();
 			   if(!empty($UserID)){
 				   $UserData = user::find($UserID);
+				   $array['User'] 							   = User::get_user_full_name();
 				  if(isset($UserData->EmailFooter) && trim($UserData->EmailFooter) != '')
 					{
 						$array['Signature']= $UserData->EmailFooter;	
