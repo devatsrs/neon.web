67a270c9b5ba7e9ea0c74bc0d38441ba1bb678d0
umer <umer.ahmed@code-desk.com>
2017-05-11 12:32:32 +0500

    Group change agent filter Fixed

--- app/controllers/TicketsSlaController.php
+++ app/controllers/TicketsSlaController.php
@@ -89,10 +89,9 @@ class TicketsSlaController extends \BaseController {
 		$targetsData				=	TicketSlaTarget::ProcessTargets($id); 
 		$slaApply					=	TicketSlaPolicyApplyTo::where(['TicketSlaID'=>$id])->first();
 		
-		$slaApplyGroup				=	explode(",",$slaApply->GroupFilter);
-		
-		$slaApplyCompany			=	explode(",",$slaApply->CompanyFilter);
-		$slaApplyType				=	explode(",",$slaApply->TypeFilter);
+		$slaApplyGroup				=	isset($slaApply->GroupFilter)?explode(",",$slaApply->GroupFilter):array();		
+		$slaApplyCompany			=	isset($slaApply->CompanyFilter)?explode(",",$slaApply->CompanyFilter):array();
+		$slaApplyType				=	isset($slaApply->TypeFilter)?explode(",",$slaApply->TypeFilter):array();
 		$RespondedVoilation			=	TicketSlaPolicyViolation::where(['TicketSlaID'=>$id,"VoilationType"=>TicketSlaPolicyViolation::$RespondedVoilationType])->select(['Time','Value'])->first();
 		
 		$ResolveVoilation			=	TicketSlaPolicyViolation::where(['TicketSlaID'=>$id,"VoilationType"=>TicketSlaPolicyViolation::$ResolvedVoilationType])->select(['Time','Value'])->get();
--- app/views/tickets/ticket_detail_dynamic_fields.blade.php
+++ app/views/tickets/ticket_detail_dynamic_fields.blade.php
@@ -196,6 +196,10 @@ var required_flds	  =          '{{json_encode($required)}}';
     
 	  $(document).on('change','#{{$htmlgroupID}}',function(e){
 		   var changeGroupID =  	$(this).val();
+		   if(changeGroupID==0){
+		   		 $('#{{$htmlagentID}} option').remove();
+				 return false;
+			}
 		   if(changeGroupID)
 		   {
 		   	 changeGroupID = parseInt(changeGroupID);
@@ -209,18 +213,18 @@ var required_flds	  =          '{{json_encode($required)}}';
 					contentType: false,
 					processData: false,
 					data:{s:1},
-					success: function(response) {	  			
+					success: function(response) { console.log(response);
+					   if(response.status =='success')
+					   {			
 						   var $el = this;		   
-						   console.log(response); 
-						  // $('#{{$htmlagentID}} option:gt(0)').remove();
-						  $('#{{$htmlagentID}} option').remove(); 
-						  var count_agents  = 0;
-						   $.each(response, function(key,value) {							  
+						   //$('#{{$htmlagentID}} option:gt(0)').remove();
+						   $('#{{$htmlagentID}} option').remove();
+						   $.each(response.data, function(key,value) {							  
 							  $('#{{$htmlagentID}}').append($("<option></option>").attr("value", value).text(key));
-							  count_agents++;
 							});					
-							if(count_agents==2){$('#{{$htmlagentID}} option').eq(1).attr("selected","selected"); }
-							$('#{{$htmlagentID}}').trigger('change');
+						}else{
+							toastr.error(response.message, "Error", toastr_opts);
+						}                   
 					}
 					});	
 		return false;		
@@ -268,6 +272,7 @@ var required_flds	  =          '{{json_encode($required)}}';
 				  	$( this ).val(0);
 				  }
             });
+		$('#{{$htmlgroupID}}').trigger('change');
 			
     });
 </script> 
