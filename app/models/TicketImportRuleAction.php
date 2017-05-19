fac92a5ed2fb5090e604c574f3a5b90107899ba8
deven <shriramsoft@gmail.com>
2017-05-19 10:17:21 +0530

    NEON-914 Dispatcher Section in ticketing

---
+++ app/models/TicketImportRuleAction.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class TicketImportRuleAction extends \Eloquent {
+
+    protected $guarded = array("TicketImportRuleActionID");
+    protected $table = 'tblTicketImportRuleAction';
+    protected $primaryKey = "TicketImportRuleActionID";
+
+
+}
