0ee22c6ad623a9402e183d43a63485060e23c991
girish vadher <girish.vadher@code-desk.com>
2017-06-02 18:57:20 +0530

    inbound rerate bug fixed

--- dbv-2/data/revisions/4.11/multiauthrule.sql
+++ dbv-2/data/revisions/4.11/multiauthrule.sql
@@ -1066,3 +1066,127 @@ BEGIN
 
 END|
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `prc_RerateInboundCalls`;
+
+DELIMITER |
+CREATE PROCEDURE `prc_RerateInboundCalls`(
+	IN `p_CompanyID` INT,
+	IN `p_processId` INT,
+	IN `p_tbltempusagedetail_name` VARCHAR(200),
+	IN `p_RateCDR` INT,
+	IN `p_RateMethod` VARCHAR(50),
+	IN `p_SpecifyRate` DECIMAL(18,6),
+	IN `p_InboundTableID` INT
+)
+BEGIN
+
+	DECLARE v_rowCount_ INT;
+	DECLARE v_pointer_ INT;
+	DECLARE v_AccountID_ INT;
+	DECLARE v_ServiceID_ INT;
+	DECLARE v_cld_ VARCHAR(500);
+
+	IF p_RateCDR = 1
+	THEN
+
+		IF (SELECT COUNT(*) FROM Ratemanagement3.tblCLIRateTable WHERE CompanyID = p_CompanyID AND RateTableID > 0) > 0
+		THEN
+
+			/* temp accounts*/
+			DROP TEMPORARY TABLE IF EXISTS tmp_Account_;
+			CREATE TEMPORARY TABLE tmp_Account_  (
+				RowID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				AccountID INT,
+				ServiceID INT,
+				cld VARCHAR(500) NULL DEFAULT NULL
+			);
+			SET @stm = CONCAT('
+			INSERT INTO tmp_Account_(AccountID,ServiceID,cld)
+			SELECT DISTINCT AccountID,ServiceID,cld FROM RMCDR3.`' , p_tbltempusagedetail_name , '` ud WHERE ProcessID="' , p_processId , '" AND AccountID IS NOT NULL AND ud.is_inbound = 1;
+			');
+
+			PREPARE stm FROM @stm;
+			EXECUTE stm;
+			DEALLOCATE PREPARE stm;
+
+		ELSEIF ( SELECT COUNT(*) FROM tmp_Service_ ) > 0
+		THEN
+
+			/* temp accounts*/
+			DROP TEMPORARY TABLE IF EXISTS tmp_Account_;
+			CREATE TEMPORARY TABLE tmp_Account_  (
+				RowID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				AccountID INT,
+				ServiceID INT,
+				cld VARCHAR(500) NULL DEFAULT NULL
+			);
+			SET @stm = CONCAT('
+			INSERT INTO tmp_Account_(AccountID,ServiceID,cld)
+			SELECT DISTINCT AccountID,ServiceID,"" FROM RMCDR3.`' , p_tbltempusagedetail_name , '` ud WHERE ProcessID="' , p_processId , '" AND AccountID IS NOT NULL AND ud.is_inbound = 1;
+			');
+
+			PREPARE stm FROM @stm;
+			EXECUTE stm;
+			DEALLOCATE PREPARE stm;
+
+		ELSE
+
+			/* temp accounts*/
+			DROP TEMPORARY TABLE IF EXISTS tmp_Account_;
+			CREATE TEMPORARY TABLE tmp_Account_  (
+				RowID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				AccountID INT,
+				ServiceID INT,
+				cld VARCHAR(500) NULL DEFAULT NULL
+			);
+			SET @stm = CONCAT('
+			INSERT INTO tmp_Account_(AccountID,ServiceID,cld)
+			SELECT DISTINCT AccountID,ServiceID,"" FROM RMCDR3.`' , p_tbltempusagedetail_name , '` ud WHERE ProcessID="' , p_processId , '" AND AccountID IS NOT NULL AND ud.is_inbound = 1;
+			');
+
+			PREPARE stm FROM @stm;
+			EXECUTE stm;
+			DEALLOCATE PREPARE stm;
+
+		END IF;
+
+		SET v_pointer_ = 1;
+		SET v_rowCount_ = (SELECT COUNT(*) FROM tmp_Account_);
+
+		IF p_InboundTableID > 0
+		THEN 
+			/* get inbound rate process*/
+			CALL Ratemanagement3.prc_getCustomerInboundRate(v_AccountID_,p_RateCDR,p_RateMethod,p_SpecifyRate,v_cld_,p_InboundTableID);
+		END IF;
+
+		WHILE v_pointer_ <= v_rowCount_
+		DO
+
+			SET v_AccountID_ = (SELECT AccountID FROM tmp_Account_ t WHERE t.RowID = v_pointer_);
+			SET v_ServiceID_ = (SELECT ServiceID FROM tmp_Account_ t WHERE t.RowID = v_pointer_);
+			SET v_cld_ = (SELECT cld FROM tmp_Account_ t WHERE t.RowID = v_pointer_);
+
+			IF p_InboundTableID =  0
+			THEN 
+
+				SET p_InboundTableID = (SELECT RateTableID FROM Ratemanagement3.tblAccountTariff  WHERE AccountID = v_AccountID_ AND ServiceID = v_ServiceID_ AND Type = 2 LIMIT 1);
+				SET p_InboundTableID = IFNULL(p_InboundTableID,0);
+				/* get inbound rate process*/
+				CALL Ratemanagement3.prc_getCustomerInboundRate(v_AccountID_,p_RateCDR,p_RateMethod,p_SpecifyRate,v_cld_,p_InboundTableID);
+			END IF;
+
+			/* update prefix inbound process*/
+			CALL prc_updateInboundPrefix(v_AccountID_, p_processId, p_tbltempusagedetail_name,v_cld_,v_ServiceID_);
+
+			/* inbound rerate process*/
+			CALL prc_updateInboundRate(v_AccountID_, p_processId, p_tbltempusagedetail_name,v_cld_,v_ServiceID_);
+
+			SET v_pointer_ = v_pointer_ + 1;
+
+		END WHILE;
+
+	END IF;
+
+END|
+DELIMITER ;
