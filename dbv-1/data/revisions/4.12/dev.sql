42c1da46678e2b45da75b6cef79a354244ef7de0
Deven Sitapara <shriramsoft@gmail.com>
2017-06-21 19:44:37 +0530

    Ticket search issue fixed

--- dbv-1/data/revisions/4.12/dev.sql
+++ dbv-1/data/revisions/4.12/dev.sql
@@ -460,3 +460,417 @@ SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
 END;;
 Delimiter ;
+
+
+-- Ticket full subject seach issue fixed
+Delimiter ;;
+DROP PROCEDURE IF EXISTS  `prc_GetSystemTicketCustomer`;
+CREATE PROCEDURE `prc_GetSystemTicketCustomer`(
+	IN `p_CompanyID` int,
+	IN `p_Search` VARCHAR(100),
+	IN `P_Status` VARCHAR(100),
+	IN `P_Priority` VARCHAR(100),
+	IN `P_Group` VARCHAR(100),
+	IN `P_Agent` VARCHAR(100),
+	IN `P_EmailAddresses` VARCHAR(200),
+	IN `p_PageNumber` INT,
+	IN `p_RowspPage` INT,
+	IN `p_lSortCol` VARCHAR(50),
+	IN `p_SortOrder` VARCHAR(5),
+	IN `p_isExport` INT
+
+)
+LANGUAGE SQL
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+COMMENT ''
+BEGIN
+	DECLARE v_OffSet_ int;
+	DECLARE v_Round_ int;
+
+	SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+	SET v_OffSet_ = (p_PageNumber * p_RowspPage) - p_RowspPage;
+
+	IF p_isExport = 0
+	THEN
+		SELECT
+			T.TicketID,
+			T.Subject,
+			CASE WHEN (ISNULL(T.RequesterName) OR T.RequesterName='')  THEN T.Requester ElSE concat(T.RequesterName," (",T.Requester,")") END as Requester,
+			T.Requester as RequesterEmail,
+			TFV.FieldValueCustomer  as TicketStatus,
+			TP.PriorityValue,
+			concat(TU.FirstName,' ',TU.LastName) as Agent,
+			T.created_at,
+			(select tc.created_at from AccountEmailLog tc where tc.TicketID = T.TicketID  and tc.EmailCall =1 and tc.EmailParent>0 order by tc.AccountEmailLogID desc limit 1) as CustomerResponse,
+		    (select tc.created_at from AccountEmailLog tc where tc.TicketID = T.TicketID  and tc.EmailCall =0  order by tc.AccountEmailLogID desc limit 1) as AgentResponse
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND (P_EmailAddresses = '' OR find_in_set(T.`Requester`,P_EmailAddresses))
+			ORDER BY
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'SubjectASC') THEN T.Subject
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'SubjectDESC') THEN T.Subject
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'StatusASC') THEN TicketStatus
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'StatusDESC') THEN TicketStatus
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'AgentASC') THEN TU.FirstName
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'AgentDESC') THEN TU.FirstName
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atASC') THEN T.created_at
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atDESC') THEN T.created_at
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'updated_atASC') THEN T.updated_at
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'updated_atDESC') THEN T.updated_at
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'RequesterASC') THEN T.Requester
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'RequesterDESC') THEN T.Requester
+			END DESC
+			LIMIT
+				p_RowspPage OFFSET v_OffSet_;
+
+		SELECT
+			COUNT(*) AS totalcount
+				FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND (P_EmailAddresses = '' OR find_in_set(T.`Requester`,P_EmailAddresses));
+
+		SELECT
+				DISTINCT(TG.GroupID),
+				TG.GroupName
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND (P_EmailAddresses = '' OR find_in_set(T.`Requester`,P_EmailAddresses))
+			AND TG.GroupName IS NOT NULL;
+
+	END IF;
+	IF p_isExport = 1
+	THEN
+	SELECT
+			T.TicketID,
+			T.Subject,
+			T.Requester,
+			T.RequesterCC as 'CC',
+			TFV.FieldValueCustomer  as 'Status',
+			TP.PriorityValue as 'Priority',
+
+			T.created_at as 'Date Created'
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND (P_EmailAddresses = '' OR find_in_set(T.`Requester`,P_EmailAddresses));
+	END IF;
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END;;
+Delimiter ;
+
+
+Delimiter ;;
+DROP PROCEDURE IF EXISTS  `prc_GetSystemTicket`;
+CREATE PROCEDURE `prc_GetSystemTicket`(
+	IN `p_CompanyID` int,
+	IN `p_Search` VARCHAR(100),
+	IN `P_Status` VARCHAR(100),
+	IN `P_Priority` VARCHAR(100),
+	IN `P_Group` VARCHAR(100),
+	IN `P_Agent` VARCHAR(100),
+	IN `P_DueBy` VARCHAR(50),
+	IN `P_CurrentDate` DATETIME,
+	IN `p_PageNumber` INT,
+	IN `p_RowspPage` INT,
+	IN `p_lSortCol` VARCHAR(50),
+	IN `p_SortOrder` VARCHAR(5),
+	IN `p_isExport` INT
+
+
+)
+LANGUAGE SQL
+NOT DETERMINISTIC
+CONTAINS SQL
+SQL SECURITY DEFINER
+COMMENT ''
+BEGIN
+	DECLARE v_OffSet_ int;
+	DECLARE v_Round_ int;
+	DECLARE v_Groups_ varchar(200);
+
+	SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+	SET v_OffSet_ = (p_PageNumber * p_RowspPage) - p_RowspPage;
+
+	IF p_isExport = 0
+	THEN
+		SELECT
+			T.TicketID,
+			T.Subject,
+
+
+  CASE
+ 	 WHEN T.AccountID>0   THEN     (SELECT CONCAT(IFNULL(TAA.AccountName,''),' (',T.Requester,')') FROM tblAccount TAA WHERE TAA.AccountID = T.AccountID  )
+  	 WHEN T.ContactID>0   THEN     (select CONCAT(IFNULL(TCCC.FirstName,''),' ',IFNULL(TCCC.LastName,''),' (',T.Requester,')') FROM tblContact TCCC WHERE TCCC.ContactID = T.ContactID)
+     WHEN T.UserID>0      THEN  	 (select CONCAT(IFNULL(TUU.FirstName,''),' ',IFNULL(TUU.LastName,''),' (',T.Requester,')') FROM tblUser TUU WHERE TUU.UserID = T.UserID )
+    ELSE CONCAT(T.RequesterName,' (',T.Requester,')')
+  END AS Requester,
+			T.Requester as RequesterEmail,
+			TFV.FieldValueAgent  as TicketStatus,
+			TP.PriorityValue,
+			concat(TU.FirstName,' ',TU.LastName) as Agent,
+			TG.GroupName,
+			T.created_at,
+
+			(select tc.created_at from AccountEmailLog tc where tc.TicketID = T.TicketID  and tc.EmailCall =1 and tc.EmailParent>0 order by tc.AccountEmailLogID desc limit 1) as CustomerResponse,
+		    (select tc.created_at from AccountEmailLog tc where tc.TicketID = T.TicketID  and tc.EmailCall =0  order by tc.AccountEmailLogID desc limit 1) as AgentResponse,
+			(select TAC.AccountID from tblAccount TAC where 	TAC.Email = T.Requester or TAC.BillingEmail =T.Requester limit 1) as ACCOUNTID,
+			T.`Read` as `Read`,
+			T.TicketSlaID,
+			T.DueDate,
+			T.updated_at,
+         T.Status,
+         T.AgentRepliedDate,
+         T.CustomerRepliedDate
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND (
+					P_DueBy = '' OR
+					(  P_DueBy != '' AND
+						(
+							   (find_in_set('Today',P_DueBy) AND DATE(T.DueDate) = DATE(P_CurrentDate))
+							OR (find_in_set('Tomorrow',P_DueBy) AND DATE(T.DueDate) =  DATE(DATE_ADD(P_CurrentDate, INTERVAL 1 Day)))
+							OR (find_in_set('Next_8_hours',P_DueBy) AND T.DueDate BETWEEN P_CurrentDate AND DATE_ADD(P_CurrentDate, INTERVAL 8 Hour))
+							OR (find_in_set('Overdue',P_DueBy) AND P_CurrentDate >=  T.DueDate )
+						)
+					)
+				)
+
+
+			ORDER BY
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'SubjectASC') THEN T.Subject
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'SubjectDESC') THEN T.Subject
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'StatusASC') THEN TicketStatus
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'StatusDESC') THEN TicketStatus
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'AgentASC') THEN TU.FirstName
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'AgentDESC') THEN TU.FirstName
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atASC') THEN T.created_at
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'created_atDESC') THEN T.created_at
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'updated_atASC') THEN T.updated_at
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'updated_atDESC') THEN T.updated_at
+			END DESC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'RequesterASC') THEN T.Requester
+			END ASC,
+			CASE
+				WHEN (CONCAT(p_lSortCol,p_SortOrder) = 'RequesterDESC') THEN T.Requester
+			END DESC
+			LIMIT
+				p_RowspPage OFFSET v_OffSet_;
+
+		SELECT
+			COUNT(*) AS totalcount
+				FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND ((P_DueBy = ''
+			OR (find_in_set('Today',P_DueBy) AND DATE(T.DueDate) = DATE(P_CurrentDate)))
+			OR (find_in_set('Tomorrow',P_DueBy) AND DATE(T.DueDate) =  DATE(DATE_ADD(P_CurrentDate, INTERVAL 1 Day)))
+			OR (find_in_set('Next_8_hours',P_DueBy) AND T.DueDate BETWEEN P_CurrentDate AND DATE_ADD(P_CurrentDate, INTERVAL 8 Hour))
+			OR (find_in_set('Overdue',P_DueBy) AND P_CurrentDate >=  T.DueDate));
+
+	SELECT
+			DISTINCT(TG.GroupID),
+			TG.GroupName
+	FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND ((P_DueBy = ''
+			OR (find_in_set('Today',P_DueBy) AND DATE(T.DueDate) = DATE(P_CurrentDate)))
+			OR (find_in_set('Tomorrow',P_DueBy) AND DATE(T.DueDate) =  DATE(DATE_ADD(P_CurrentDate, INTERVAL 1 Day)))
+			OR (find_in_set('Next_8_hours',P_DueBy) AND T.DueDate BETWEEN P_CurrentDate AND DATE_ADD(P_CurrentDate, INTERVAL 8 Hour))
+			OR (find_in_set('Overdue',P_DueBy) AND P_CurrentDate >=  T.DueDate ) );
+
+	END IF;
+	IF p_isExport = 1
+	THEN
+	SELECT
+			T.TicketID,
+			T.Subject,
+			T.Requester,
+			T.RequesterCC as 'CC',
+			TFV.FieldValueAgent  as 'Status',
+			TP.PriorityValue as 'Priority',
+			concat(TU.FirstName,' ',TU.LastName) as Agent,
+			T.created_at as 'Date Created',
+			TG.GroupName as 'Group'
+		FROM
+			tblTickets T
+		LEFT JOIN tblTicketfieldsValues TFV
+			ON TFV.ValuesID = T.Status
+		LEFT JOIN tblTicketPriority TP
+			ON TP.PriorityID = T.Priority
+		LEFT JOIN tblUser TU
+			ON TU.UserID = T.Agent
+		LEFT JOIN tblTicketGroups TG
+			ON TG.GroupID = T.`Group`
+		LEFT JOIN tblContact TCC
+			ON TCC.Email = T.`Requester`
+
+		WHERE
+			T.CompanyID = p_CompanyID
+			AND (p_Search = '' OR ( p_Search != '' AND (T.Subject like Concat('%',p_Search,'%') OR  T.Description like Concat('%',p_Search,'%') OR  T.Requester like Concat('%',p_Search,'%') OR  T.RequesterName like Concat('%',p_Search,'%') ) OR (T.TicketID in  ( select ael.TicketID from AccountEmailLog ael where (ael.Subject like Concat('%',p_Search,'%') OR  ael.Emailfrom like Concat('%',p_Search,'%') OR  ael.EmailfromName like Concat('%',p_Search,'%') OR  ael.Message like Concat('%',p_Search,'%') )   ) ) ) )
+			AND (P_Status = '' OR find_in_set(T.`Status`,P_Status))
+			AND (P_Priority = '' OR find_in_set(T.`Priority`,P_Priority))
+			AND (P_Group = '' OR find_in_set(T.`Group`,P_Group))
+			AND (P_Agent = '' OR find_in_set(T.`Agent`,P_Agent))
+			AND ((P_DueBy = ''
+			OR (find_in_set('Today',P_DueBy) AND DATE(T.DueDate) = DATE(P_CurrentDate)))
+			OR (find_in_set('Tomorrow',P_DueBy) AND DATE(T.DueDate) =  DATE(DATE_ADD(P_CurrentDate, INTERVAL 1 Day)))
+			OR (find_in_set('Next_8_hours',P_DueBy) AND T.DueDate BETWEEN P_CurrentDate AND DATE_ADD(P_CurrentDate, INTERVAL 8 Hour))
+			OR (find_in_set('Overdue',P_DueBy) AND P_CurrentDate >=  T.DueDate ));
+	END IF;
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END;;
+Delimiter ;
+
+
