8f3d20c49fb7449a1e2943ebe493e88d5ce7b452
girish vadher <girish.vadher@code-desk.com>
2017-07-21 15:22:25 +0530

    #765 - Dynamic Reporting

--- app/views/report/filter.blade.php
+++ app/views/report/filter.blade.php
@@ -331,7 +331,7 @@
             <form id="add-new-report-form" method="post">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Add New Report</h4>
+                    <h4 class="modal-title">{{isset($report->ReportID)?'Add New':'Edit'}} Report</h4>
                 </div>
                 <div class="modal-body">
                     <div class="row margin-top">
