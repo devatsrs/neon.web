d3f76f58609d357dc3a8a0bf394f422b547a1564
bhavin machhoya <bhavin@code-desk.com>
2017-10-27 17:09:15 +0530

    Add All - Vos 3.2 #1188

--- dbv-1/data/revisions/4.15/bhavin.sql
+++ dbv-1/data/revisions/4.15/bhavin.sql
@@ -1541,3 +1541,252 @@ BEGIN
       SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 END//
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `prc_WSGenerateVendorVersion3VosSheet`;
+DELIMITER //
+CREATE PROCEDURE `prc_WSGenerateVendorVersion3VosSheet`(
+	IN `p_VendorID` INT ,
+	IN `p_Trunks` varchar(200) ,
+	IN `p_Effective` VARCHAR(50),
+	IN `p_Format` VARCHAR(50)
+)
+BEGIN
+         SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+        call vwVendorVersion3VosSheet(p_VendorID,p_Trunks,p_Effective);
+
+        IF p_Effective = 'Now' OR p_Format = 'Vos 2.0'
+		  THEN
+
+	        SELECT  `Rate Prefix` ,
+	                `Area Prefix` ,
+	                `Rate Type` ,
+	                `Area Name` ,
+	                `Billing Rate` ,
+	                `Billing Cycle`,
+	                `Minute Cost` ,
+	                `Lock Type` ,
+	                `Section Rate` ,
+	                `Billing Rate for Calling Card Prompt` ,
+	                `Billing Cycle for Calling Card Prompt`
+	        FROM    tmp_VendorVersion3VosSheet_
+	        WHERE   AccountID = p_VendorID
+	        AND  FIND_IN_SET(TrunkId,p_Trunks)!= 0
+	        ORDER BY `Rate Prefix`;
+
+        END IF;
+
+        IF p_Effective = 'Future' AND p_Format = 'Vos 3.2'
+		  THEN
+
+	        SELECT  CONCAT(tmp_VendorVersion3VosSheet_.EffectiveDate,' 00:00') as `Time of timing replace`,
+					'Append replace' as `Mode of timing replace`,
+			  		`Rate Prefix` ,
+	                `Area Prefix` ,
+	                `Rate Type` ,
+	                `Area Name` ,
+	                `Billing Rate` ,
+	                `Billing Cycle`,
+	                `Minute Cost` ,
+	                `Lock Type` ,
+	                `Section Rate` ,
+	                `Billing Rate for Calling Card Prompt` ,
+	                `Billing Cycle for Calling Card Prompt`
+	        FROM    tmp_VendorVersion3VosSheet_
+	        WHERE   AccountID = p_VendorID
+	        AND  FIND_IN_SET(TrunkId,p_Trunks)!= 0
+	        ORDER BY `Rate Prefix`;
+
+        END IF;
+        
+        
+
+        IF p_Effective = 'All' AND p_Format = 'Vos 3.2'
+		  THEN
+
+	        SELECT  CONCAT(tmp_VendorVersion3VosSheet_.EffectiveDate,' 00:00') as `Time of timing replace`,
+					'Append replace' as `Mode of timing replace`,
+			  		`Rate Prefix` ,
+	                `Area Prefix` ,
+	                `Rate Type` ,
+	                `Area Name` ,
+	                `Billing Rate` ,
+	                `Billing Cycle`,
+	                `Minute Cost` ,
+	                `Lock Type` ,
+	                `Section Rate` ,
+	                `Billing Rate for Calling Card Prompt` ,
+	                `Billing Cycle for Calling Card Prompt`
+	        FROM    tmp_VendorVersion3VosSheet_
+	        WHERE   AccountID = p_VendorID
+	        AND  FIND_IN_SET(TrunkId,p_Trunks)!= 0
+	        ORDER BY `Rate Prefix`;
+
+        END IF;
+
+        SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END//
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS `prc_WSGenerateVersion3VosSheet`;
+DELIMITER //
+CREATE PROCEDURE `prc_WSGenerateVersion3VosSheet`(
+	IN `p_CustomerID` INT ,
+	IN `p_trunks` varchar(200) ,
+	IN `p_Effective` VARCHAR(50),
+	IN `p_Format` VARCHAR(50)
+)
+BEGIN
+
+    DECLARE v_codedeckid_ INT;
+    DECLARE v_ratetableid_ INT;
+    DECLARE v_RateTableAssignDate_ DATETIME;
+    DECLARE v_NewA2ZAssign_ INT;
+    DECLARE v_companyid_ INT;
+    DECLARE v_TrunkID_ INT;
+    DECLARE v_pointer_ INT ;
+    DECLARE v_rowCount_ INT ;
+
+    SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+    
+    DROP TEMPORARY TABLE IF EXISTS tmp_customerrateall_;
+    CREATE TEMPORARY TABLE tmp_customerrateall_ (
+        RateID INT,
+        Code VARCHAR(50),
+        Description VARCHAR(200),
+        Interval1 INT,
+        IntervalN INT,
+        ConnectionFee DECIMAL(18, 6),
+        RoutinePlanName VARCHAR(50),
+        Rate DECIMAL(18, 6),
+        EffectiveDate DATE,
+        LastModifiedDate DATETIME,
+        LastModifiedBy VARCHAR(50),
+        CustomerRateId INT,
+        TrunkID INT,
+        RateTableRateId INT,
+        IncludePrefix TINYINT,
+        Prefix VARCHAR(50),
+        RatePrefix VARCHAR(50),
+        AreaPrefix VARCHAR(50)
+    );
+
+    DROP TEMPORARY TABLE IF EXISTS tmp_trunks_;
+    CREATE TEMPORARY TABLE tmp_trunks_  (
+        TrunkID INT,
+        RowNo INT
+    );
+
+
+    SELECT
+        CompanyId INTO v_companyid_
+    FROM tblAccount
+    WHERE AccountID = p_CustomerID;
+
+
+
+    INSERT INTO tmp_trunks_
+    SELECT TrunkID,
+        @row_num := @row_num+1 AS RowID
+    FROM tblCustomerTrunk,(SELECT @row_num := 0) x
+    WHERE  FIND_IN_SET(tblCustomerTrunk.TrunkID,p_Trunks)!= 0
+        AND tblCustomerTrunk.AccountID = p_CustomerID;
+
+    SET v_pointer_ = 1;
+    SET v_rowCount_ = (SELECT COUNT(*)FROM tmp_trunks_);
+
+
+    WHILE v_pointer_ <= v_rowCount_
+    DO
+
+        SET v_TrunkID_ = (SELECT TrunkID FROM tmp_trunks_ t WHERE t.RowNo = v_pointer_);
+
+        CALL prc_GetCustomerRate(v_companyid_,p_CustomerID,v_TrunkID_,null,null,null,p_Effective,1,0,0,0,'','',-1);
+
+        INSERT INTO tmp_customerrateall_
+        SELECT * FROM tmp_customerrate_;
+
+        SET v_pointer_ = v_pointer_ + 1;
+    END WHILE;
+
+    	IF p_Effective = 'Now' OR p_Format = 'Vos 2.0'
+		  THEN
+
+        SELECT distinct
+                IFNULL(RatePrefix, '') as `Rate Prefix` ,
+                Concat(IFNULL(AreaPrefix,''), Code) as `Area Prefix` ,
+                'International' as `Rate Type` ,
+                Description  as `Area Name`,
+                Rate / 60  as `Billing Rate`,
+                IntervalN as `Billing Cycle`,
+                Rate as `Minute Cost` ,
+                'No Lock'  as `Lock Type`,
+                CASE WHEN Interval1 != IntervalN
+               	 THEN Concat('0,', Rate, ',',Interval1)
+                ELSE
+					 	 ''
+                END as `Section Rate`,
+                0 AS `Billing Rate for Calling Card Prompt`,
+                0  as `Billing Cycle for Calling Card Prompt`
+        FROM   tmp_customerrateall_
+        ORDER BY `Rate Prefix`;
+
+		 END IF;
+
+	 	IF p_Effective = 'Future' AND p_Format = 'Vos 3.2'
+		  THEN
+
+        SELECT distinct
+        		CONCAT(EffectiveDate,' 00:00') as `Time of timing replace`,
+        		'Append replace' as `Mode of timing replace`,
+                IFNULL(RatePrefix, '') as `Rate Prefix` ,
+                Concat(IFNULL(AreaPrefix,''), Code) as `Area Prefix` ,
+                'International' as `Rate Type` ,
+                Description  as `Area Name`,
+                Rate / 60  as `Billing Rate`,
+                IntervalN as `Billing Cycle`,
+                Rate as `Minute Cost` ,
+                'No Lock'  as `Lock Type`,
+                CASE WHEN Interval1 != IntervalN
+               	 THEN Concat('0,', Rate, ',',Interval1)
+                ELSE
+					 	 ''
+                END as `Section Rate`,
+                0 AS `Billing Rate for Calling Card Prompt`,
+                0  as `Billing Cycle for Calling Card Prompt`
+        FROM   tmp_customerrateall_
+        ORDER BY `Rate Prefix`;
+
+		 END IF;
+		 
+		 IF p_Effective = 'All' AND p_Format = 'Vos 3.2'
+		  THEN
+
+        SELECT distinct
+      			CONCAT(EffectiveDate,' 00:00') as `Time of timing replace`,
+        		'Append replace' as `Mode of timing replace`,
+                IFNULL(RatePrefix, '') as `Rate Prefix` ,
+                Concat(IFNULL(AreaPrefix,''), Code) as `Area Prefix` ,
+                'International' as `Rate Type` ,
+                Description  as `Area Name`,
+                Rate / 60  as `Billing Rate`,
+                IntervalN as `Billing Cycle`,
+                Rate as `Minute Cost` ,
+                'No Lock'  as `Lock Type`,
+                CASE WHEN Interval1 != IntervalN
+               	 THEN Concat('0,', Rate, ',',Interval1)
+                ELSE
+					 	 ''
+                END as `Section Rate`,
+                0 AS `Billing Rate for Calling Card Prompt`,
+                0  as `Billing Cycle for Calling Card Prompt`
+        FROM   tmp_customerrateall_
+        ORDER BY `Rate Prefix`;
+
+		 END IF;
+
+   SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+END//
+DELIMITER ;
