064e3a567f2e4914e8d7e708dd9357c37f23ecc6
girish vadher <girish.vadher@code-desk.com>
2017-11-20 16:59:44 +0530

    #1194  margin report

--- dbv-4/data/revisions/4.15/reports.sql
+++ dbv-4/data/revisions/4.15/reports.sql
@@ -456,8 +456,9 @@ BEGIN
 
 	SET @stmt = CONCAT('
 	UPDATE tmp_tblVendorUsageDetailsReport_' , p_UniqueID , ' vd 
-   INNER JOIN tmp_tblUsageDetailsReport_' , p_UniqueID , ' cd ON cd.CompanyGatewayID = vd.CompanyGatewayID AND cd.ID = vd.ID
-   	SET cd.VAccountID = vd.VAccountID,cd.GatewayVAccountPKID = vd.GatewayVAccountPKID,cd.call_status_v = vd.call_status_v,cd.buying_cost =vd.buying_cost;
+	INNER JOIN tmp_tblUsageDetailsReport_' , p_UniqueID , ' cd ON cd.CompanyGatewayID = vd.CompanyGatewayID AND cd.ID = vd.ID
+		SET cd.VAccountID = vd.VAccountID,cd.GatewayVAccountPKID = vd.GatewayVAccountPKID,cd.call_status_v = vd.call_status_v,cd.buying_cost =vd.buying_cost
+	WHERE vd.buying_cost <> 0;
 	');
 
 	PREPARE stmt FROM @stmt;
