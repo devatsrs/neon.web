f00b38b00ff4463d7c02cf45ff6f389d02632138
girish vadher <girish.vadher@code-desk.com>
2017-12-08 12:00:09 +0530

    #1227 Merge Summary Jobs

--- dbv-1/data/revisions/4.16/girish.sql
+++ dbv-1/data/revisions/4.16/girish.sql
@@ -2,4 +2,7 @@ USE Ratemanagement3;
 
 update tblCronJob set Status = 0 where CronJobCommandID IN (SELECT CronJobCommandID FROM tblCronJobCommand where Command = 'createvendorsummary');
 update tblCronJobCommand set Status = 0 where Command = 'createvendorsummary';
+
+update tblCronJobCommand set Title = 'Create Summary' where Command = 'createsummary';
+update tblCronJob set JobTitle = 'Create Summary' where CronJobCommandID IN (SELECT CronJobCommandID FROM tblCronJobCommand where Command = 'createsummary');
 update tblCronJobCommand set Settings = '[[{"title":"Start Date","type":"text","datepicker":"","value":"","name":"StartDate"},{"title":"End Date","type":"text","value":"","datepicker":"","name":"EndDate"},{"title":"Success Email","type":"text","value":"","name":"SuccessEmail"},{"title":"Error Email","type":"text","value":"","name":"ErrorEmail"},{"title":"Threshold Time (Minute)","type":"text","value":"","name":"ThresholdTime"}]]' where Command = 'createsummary';
