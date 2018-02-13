35789b0e99f255882f97bbb07dae86ea26473f90
Deven Sitapara <shriramsoft@gmail.com>
2018-02-13 17:56:34 +0530

    NEON-1361 CLI based authentication on service

--- dbv-2/data/revisions/4.17/dev.sql
+++ dbv-2/data/revisions/4.17/dev.sql
@@ -570,3 +570,71 @@ BEGIN
 
 END//
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `prc_ProcessCDRService`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_ProcessCDRService`(
+	IN `p_CompanyID` INT,
+	IN `p_processId` INT,
+	IN `p_tbltempusagedetail_name` VARCHAR(200)
+
+
+
+)
+BEGIN
+
+	DECLARE v_ServiceAccountID_CLI_Count_ INT;
+	DECLARE v_ServiceAccountID_IP_Count_ INT;
+
+	SELECT COUNT(*) INTO v_ServiceAccountID_CLI_Count_
+	FROM Ratemanagement3.tblAccountAuthenticate aa
+	INNER JOIN Ratemanagement3.tblCLIRateTable crt ON crt.AccountID = aa.AccountID
+	WHERE aa.CompanyID = p_CompanyID AND (CustomerAuthRule = 'CLI' OR VendorAuthRule = 'CLI') AND crt.ServiceID > 0 ;
+
+	IF v_ServiceAccountID_CLI_Count_ > 0
+	THEN
+
+
+		SET @stm = CONCAT('
+		UPDATE RMCDR3.`' , p_tbltempusagedetail_name , '` uh
+		INNER JOIN Ratemanagement3.tblCLIRateTable ga
+			ON  ga.CompanyID = uh.CompanyID
+			AND ga.CLI = uh.cli
+
+		SET uh.ServiceID = ga.ServiceID
+		WHERE ga.ServiceID > 0
+		AND uh.CompanyID = ' ,  p_CompanyID , '
+		AND uh.ProcessID = "' , p_processId , '" ;
+		');
+		PREPARE stmt FROM @stm;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;
+
+	END IF;
+
+	SELECT COUNT(*) INTO v_ServiceAccountID_IP_Count_
+	FROM Ratemanagement3.tblAccountAuthenticate aa
+	WHERE aa.CompanyID = p_CompanyID AND (CustomerAuthRule = 'IP' OR VendorAuthRule = 'IP') AND ServiceID > 0;
+
+	IF v_ServiceAccountID_IP_Count_ > 0
+	THEN
+
+
+		SET @stm = CONCAT('
+		UPDATE RMCDR3.`' , p_tbltempusagedetail_name , '` uh
+		INNER JOIN Ratemanagement3.tblAccountAuthenticate ga
+			ON  ga.CompanyID = uh.CompanyID
+			AND ( FIND_IN_SET(uh.GatewayAccountID,ga.CustomerAuthValue) != 0 OR FIND_IN_SET(uh.GatewayAccountID,ga.VendorAuthValue) != 0 )
+		SET uh.ServiceID = ga.ServiceID
+		WHERE ga.ServiceID > 0
+		AND uh.CompanyID = ' ,  p_CompanyID , '
+		AND uh.ProcessID = "' , p_processId , '" ;
+		');
+		PREPARE stmt FROM @stm;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;
+
+	END IF;
+
+END//
+DELIMITER ;
