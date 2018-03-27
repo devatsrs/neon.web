f34f3dfa0ae90e2e44def28ed1d2c0e879e79da7
girish vadher <girish.vadher@code-desk.com>
2018-03-27 12:19:12 +0530

    RateTable Apply To Multi Account Task.

--- app/routes.php
+++ app/routes.php
@@ -683,6 +683,10 @@ Route::group(array('before' => 'auth'), function () {
 	Route::controller('rategenerators', 'RateGeneratorsController');
 
 	//RateTables
+	Route::any('/rate_tables/multiaccounts', array('as' => 'customer_rates', 'uses' => 'RateTablesMultiAccController@index'));
+	Route::any('/rate_tables/multiaccounts/store', 'RateTablesMultiAccController@store');
+	Route::any('/rate_tables/multiaccounts/ajax_datagrid/{type}', 'RateTablesMultiAccController@ajax_datagrid');
+
     Route::any('/rate_tables', array('as' => 'customer_rates', 'uses' => 'RateTablesController@index'));
 	Route::any('/rate_tables/{id}/search_ajax_datagrid', array('as' => 'customer_rates_search', 'uses' => 'RateTablesController@search_ajax_datagrid'));
 	Route::any('/rate_tables/ajax_datagrid', 'RateTablesController@ajax_datagrid');
