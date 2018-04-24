7e41e199afa2bd94883ba5f7b58fb935b8d37bb4
Vasim <Vasim>
2018-04-24 17:07:56 +0530

    [change] Upload Templates Code improved, and added some functionalities, moved code from file to database

--- app/models/FileUploadTemplate.php
+++ app/models/FileUploadTemplate.php
@@ -11,17 +11,17 @@ class FileUploadTemplate extends \Eloquent {
     protected $guarded = array();
     protected $table = 'tblFileUploadTemplate';
     protected $primaryKey = "FileUploadTemplateID";
-    const TEMPLATE_CDR = 'CDR';
-    const TEMPLATE_VENDORCDR = 'VendorCDR';
-    const TEMPLATE_Account = 'Account';
-    const TEMPLATE_Leads = 'Leads';
-    const TEMPLATE_DIALSTRING = 'DialString';
-    const TEMPLATE_IPS = 'IPs';
-    const TEMPLATE_ITEM = 'Item';
-    const TEMPLATE_VENDOR_RATE = 'VendorRate';
-    const TEMPLATE_PAYMENT = 'Payment';
-    const TEMPLATE_RATETABLE_RATE = 'RatetableRate';
-    const TEMPLATE_CUSTOMER_RATE = 'CustomerRate';
+    const TEMPLATE_CDR              = 'CDR';
+    const TEMPLATE_VENDORCDR        = 'VendorCDR';
+    const TEMPLATE_Account          = 'Account';
+    const TEMPLATE_Leads            = 'Leads';
+    const TEMPLATE_DIALSTRING       = 'DialString';
+    const TEMPLATE_IPS              = 'IPs';
+    const TEMPLATE_ITEM             = 'Item';
+    const TEMPLATE_VENDOR_RATE      = 'VendorRate';
+    const TEMPLATE_PAYMENT          = 'Payment';
+    const TEMPLATE_RATETABLE_RATE   = 'RatetableRate';
+    const TEMPLATE_CUSTOMER_RATE    = 'CustomerRate';
 
     public static function getTemplateIDList($Type){
         if(!empty($Type)) {
