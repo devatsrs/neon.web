34dc434d25a295e14bf2f0350390054cb5a528d4
Deven Sitapara <shriramsoft@gmail.com>
2018-05-10 12:53:38 +0530

    NEON-1383 FTP CDR Download Check - ReRate

--- app/models/UsageDetail.php
+++ app/models/UsageDetail.php
@@ -10,4 +10,10 @@ class UsageDetail extends \Eloquent {
 
     protected  $primaryKey = "UsageDetailID";
 
+
+    const RATE_METHOD_CURRENT_RATE = "CurrentRate";
+    const RATE_METHOD_SPECIFYRATE = "SpecifyRate";
+    const RATE_METHOD_VALUE_AGAINST_COST = "ValueAgainstCost";
+
+    static $RateMethod = array(RATE_METHOD_CURRENT_RATE=>'Rate setup against account',RATE_METHOD_SPECIFYRATE=>'Specify Rate',RATE_METHOD_VALUE_AGAINST_COST=> "Add Margin on Cost" );
 }
