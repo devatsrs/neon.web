86d5dbbb3822d748aedd52c02e4920519a49a740
bhavin machhoya <bhavin@code-desk.com>
2018-05-31 16:33:54 +0530

    Service Ticket Issue (31-05-2018)

--- app/views/tickets/detail.blade.php
+++ app/views/tickets/detail.blade.php
@@ -62,7 +62,7 @@
       <!-- panel body -->
       <div @if($loop>4) style="display:none;" @endif  class="panel-body"> {{$TicketConversationData->EmailMessage}}
         <?php $attachments = unserialize($TicketConversationData->AttachmentPaths);  ?>
-        @if(count($attachments)>0 && strlen($TicketConversationData->AttachmentPaths)>0)
+        @if(!empty($attachments) && count($attachments)>0 && strlen($TicketConversationData->AttachmentPaths)>0)
         <div class="mail-attachments last_data">
           <h4> <i class="entypo-attach"></i> Attachments <span>({{count($attachments)}})</span> </h4>
           <ul>
