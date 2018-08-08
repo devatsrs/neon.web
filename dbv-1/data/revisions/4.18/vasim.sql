70bf3dab75655cc7b9f8d05444b89e864be52258
vasim seta <vasim.seta@codedesk.com>
2018-08-08 11:16:44 +0530

    [fix] Data too long for column in fnFIND_IN_SET (NEON-1447)

--- dbv-1/data/revisions/4.18/vasim.sql
+++ dbv-1/data/revisions/4.18/vasim.sql
@@ -16114,3 +16114,52 @@ BEGIN
 
 END//
 DELIMITER ;
+
+
+
+
+DROP FUNCTION IF EXISTS `fnFIND_IN_SET`;
+DELIMITER //
+CREATE FUNCTION `fnFIND_IN_SET`(
+	`p_String1` LONGTEXT,
+	`p_String2` LONGTEXT
+) RETURNS int(11)
+BEGIN
+	DECLARE i int;
+	DECLARE counter int;
+
+	DROP TEMPORARY TABLE IF EXISTS `table1`;
+	CREATE TEMPORARY TABLE `table1` (
+	  `splitted_column` TEXT NOT NULL
+	);
+
+	DROP TEMPORARY TABLE IF EXISTS `table2`;
+	CREATE TEMPORARY TABLE `table2` (
+	  `splitted_column` TEXT NOT NULL
+	);
+
+
+	SET i = 1;
+	REPEAT
+		INSERT INTO table1
+		SELECT FnStringSplit(p_String1, ',', i) WHERE FnStringSplit(p_String1, ',', i) IS NOT NULL LIMIT 1;
+		SET i = i + 1;
+		UNTIL ROW_COUNT() = 0
+	END REPEAT;
+
+	SET i = 1;
+	REPEAT
+		INSERT INTO table2
+		SELECT FnStringSplit(p_String2, ',', i) WHERE FnStringSplit(p_String2, ',', i) IS NOT NULL LIMIT 1;
+		SET i = i + 1;
+		UNTIL ROW_COUNT() = 0
+	END REPEAT;
+
+	SELECT COUNT(*) INTO counter
+	FROM table1
+	INNER JOIN table2 ON table1.splitted_column = table2.splitted_column;
+
+	RETURN counter;
+
+END//
+DELIMITER ;
