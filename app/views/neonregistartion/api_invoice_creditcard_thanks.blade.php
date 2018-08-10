0c95c70585d1438ca3e067603b42868bf7a9832f
bhavin machhoya <bhavin@code-desk.com>
2018-08-10 13:12:09 +0530

    Neon API for registration widget #1470

--- app/views/neonregistartion/api_invoice_creditcard_thanks.blade.php
+++ app/views/neonregistartion/api_invoice_creditcard_thanks.blade.php
@@ -56,4 +56,63 @@
 
     });
 </script>
+<script>
+    toastr_opts = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    if ($.isFunction($.fn.select2))
+    {
+        $("select.select2").each(function(i, el)
+        {
+            var $this = $(el),
+                    opts = {
+                        allowClear: attrDefault($this, 'allowClear', false)
+                    };
+            if($this.hasClass('small')){
+                opts['minimumResultsForSearch'] = attrDefault($this, 'allowClear', Infinity);
+                opts['dropdownCssClass'] = attrDefault($this, 'allowClear', 'no-search')
+            }
+            $this.select2(opts);
+            if($this.hasClass('small')){
+                $this.select2('container').find('.select2-search').addClass ('hidden') ;
+            }
+            //$this.select2("open");
+        }).promise().done(function(){
+            $('.select2').css('visibility','visible');
+        });
+
+
+        if ($.isFunction($.fn.perfectScrollbar))
+        {
+            $(".select2-results").niceScroll({
+                cursorcolor: '#d4d4d4',
+                cursorborder: '1px solid #ccc',
+                railpadding: {right: 3}
+            });
+        }
+    }
+
+    // Element Attribute Helper
+    function attrDefault($el, data_var, default_val)
+    {
+        if (typeof $el.data(data_var) != 'undefined')
+        {
+            return $el.data(data_var);
+        }
+
+        return default_val;
+    }
+</script>
 @stop
