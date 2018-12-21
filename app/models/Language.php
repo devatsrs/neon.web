8d62bf97b70739302a156c3182baac8082bfd700
aamarnazir <aamarnazir@code-desk.com>
2018-12-21 08:14:15 -0800

    SI1-15 - Product Apis

---
+++ app/models/Language.php
@@ -0,0 +1,14 @@
+<?php
+
+class Language extends \Eloquent
+{
+    protected $guarded = array("LanguageID");
+
+    protected $table = 'tblLanguage';
+
+    protected $primaryKey = "LanguageID";
+
+
+
+
+}
