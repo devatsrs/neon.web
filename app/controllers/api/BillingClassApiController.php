72c500abcc79c9bf8ece60b0957f820282a3ab41
jenil ramavat <jenil.srs@gmail.com>
2018-12-24 14:30:45 +0530

    Balance Flow APIs - Stage 1 - SI1-62

--- app/controllers/api/BillingClassApiController.php
+++ app/controllers/api/BillingClassApiController.php
@@ -45,20 +45,24 @@ class BillingClassApiController extends ApiController {
 				//Update
 				$BillingClass = BillingClass::findOrFail($data['BillingClassID']);
 
-				$LowBalanceReminderSettings = json_decode($BillingClass->LowBalanceReminderSettings);
-				if (isset($LowBalanceReminderSettings->LastRunTime)) {
-					$PostData['LowBalanceReminderSettings']['LastRunTime'] = $LowBalanceReminderSettings->LastRunTime;
+				if(!empty($BillingClass)){
+					$LowBalanceReminderSettings = json_decode($BillingClass->LowBalanceReminderSettings);
+					if (isset($LowBalanceReminderSettings->LastRunTime)) {
+						$PostData['LowBalanceReminderSettings']['LastRunTime'] = $LowBalanceReminderSettings->LastRunTime;
+					}
+					if (isset($LowBalanceReminderSettings->NextRunTime)) {
+						$PostData['LowBalanceReminderSettings']['NextRunTime'] = $LowBalanceReminderSettings->NextRunTime;
+					}
+
+					$PostData['LowBalanceReminderSettings'] = json_encode($PostData['LowBalanceReminderSettings']);
+					$PostData['UpdatedBy'] = 'API';
+
+					$BillingClass->update($PostData);
+					return Response::json(["status"=>"success", "Message"=>"Updated Successfully."]);
+				}else{
+					return Response::json(["status"=>"failed", "Message"=>"Billing Class Not Found."]);
 				}
-				if (isset($LowBalanceReminderSettings->NextRunTime)) {
-					$PostData['LowBalanceReminderSettings']['NextRunTime'] = $LowBalanceReminderSettings->NextRunTime;
-				}
-
-				$PostData['LowBalanceReminderSettings'] = json_encode($PostData['LowBalanceReminderSettings']);
-				$PostData['UpdatedBy'] = 'API';
-
-				$BillingClass->update($PostData);
-				return Response::json(["status"=>"success", "Message"=>"Updated Successfully."]);
-
+				
 			} else {
 				// Create
 				$rules = array(
