7ee5e20bc3da673a85fd2cacf375f3cc0cbdb32b
jenil ramavat <jenil.srs@gmail.com>
2019-01-22 18:38:05 +0530

    auto top up api - SI1-86

--- app/controllers/api/CountryApiController.php
+++ app/controllers/api/CountryApiController.php
@@ -6,7 +6,7 @@ class CountryApiController extends ApiController {
 	public function getList()
 	{
 
-		$Countries = Country::select(["CountryID","Prefix","Country", "ISO2", "ISO3", "Keywords"])
+		$Countries = Country::select(["CountryID","Prefix","Country", "ISO2", "ISO3"])
 			->get();
 		return Response::json(["status"=>"success", "data"=>$Countries]);
 	}
--- app/controllers/api/LanguageApiController.php
+++ app/controllers/api/LanguageApiController.php
@@ -6,7 +6,7 @@ class LanguageApiController extends ApiController {
 	public function getList()
 	{
 
-		$Languages = Language::select(["LanguageID","ISOCode","Language", "flag", "is_rtl"])
+		$Languages = Language::select(["LanguageID","ISOCode","Language"])
 			->get();
 		return Response::json(["status"=>"success", "data"=>$Languages]);
 	}
