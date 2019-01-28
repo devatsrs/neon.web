5fa73cb5a3a67c0c969c287946a3fa7bd355ca99
jenil ramavat <jenil.srs@gmail.com>
2019-01-28 13:23:01 +0530

    http status codes change

--- app/controllers/api/Codes.php
+++ app/controllers/api/Codes.php
@@ -12,7 +12,8 @@ class Codes
     public static $Code500 = ["400","Bad Request"];
     public static $Code401 =["401","Unauthorized"];
     public static $Code402 =["402","Invalid parameters"];
-    public static $Code403 = ["403","Not found"];
+    public static $Code403 = ["403","Forbidden"];
+    public static $Code404 = ["404","Not Found"];
     public static $Code410 = ["410","Record already exists"];
     public static $Code409 = ["409","Conflict"];
 
