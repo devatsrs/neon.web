1dbbd366ea7868e55c68f80fe45a895bd17f114e
aamarnazir <aamarnazir@code-desk.com>
2019-02-12 04:03:22 -0800

    SI1-141 - Update AccountAPI

--- app/controllers/api/AccountsApiController.php
+++ app/controllers/api/AccountsApiController.php
@@ -1483,77 +1483,84 @@ class AccountsApiController extends ApiController {
 			$data['AccountID'] = $accountInfo->AccountID;
 			$data['CompanyID'] =$accountInfo->CompanyId;
 			$data['Number'] =$accountInfo->Number;
-			if (isset($accountData['FirstName'])) {
+			if (isset($accountData['FirstName']) && !empty($accountData['FirstName'])) {
 				$data['FirstName'] = $accountData['FirstName'];
 			}
-			if (isset($accountData['LastName'])) {
+			if (isset($accountData['LastName']) && !empty($accountData['LastName'])) {
 				$data['LastName'] = $accountData['LastName'];
 			}
-			if (isset($accountData['Phone'])) {
+			if (isset($accountData['Phone']) && !empty($accountData['Phone'])) {
 				$data['Phone'] = $accountData['Phone'];
 			}
-			if (isset($accountData['Address1'])) {
+			if (isset($accountData['Address1']) && !empty($accountData['Address1'])) {
 				$data['Address1'] = $accountData['Address1'];
 			}
-			if (isset($accountData['Address2'])) {
+			if (isset($accountData['Address2']) && !empty($accountData['Address2'])) {
 				$data['Address2'] = $accountData['Address2'];
 			}
-			if (isset($accountData['City'])) {
+			if (isset($accountData['City']) && !empty($accountData['City'])) {
 				$data['City'] = $accountData['City'];
 			}
-			if (isset($accountData['Email'])) {
+			if (isset($accountData['Email']) && !empty($accountData['Email'])) {
 				$data['Email'] = $accountData['Email'];
 			}
-			if (isset($accountData['BillingEmail'])) {
+			if (isset($accountData['BillingEmail']) && !empty($accountData['BillingEmail'])) {
 				$data['BillingEmail'] = $accountData['BillingEmail'];
 			}
 
 			$BillingSetting['billing_class']= isset($accountData['BillingClassID']) ? $accountData['BillingClassID'] : '';
-			$data['AccountName']= isset($accountData['AccountName']) ? $accountData['AccountName'] : '';
-			$data['CurrencyId'] = isset($accountData['CurrencySymbol']) ? $accountData['CurrencySymbol'] : '';
-			$data['Country'] = isset($accountData['CountryIso2']) ? $accountData['CountryIso2'] : '';
-			$data['password'] = isset($accountData['CustomerPanelPassword']) ? Crypt::encrypt($accountData['CustomerPanelPassword']) :'';
-			$data['VatNumber'] = isset($accountData['VatNumber']) ? $accountData['VatNumber'] : '';
-			$data['Language']= isset($accountData['LanguageIso2']) ? $accountData['LanguageIso2'] : '';
+			if (isset($accountData['AccountName']) && !empty($accountData['AccountName'])) {
+				$data['AccountName'] = $accountData['AccountName'];
+				if (strpbrk($data['AccountName'], '\/?*:|"<>')) {
+					return Response::json(array("status" => Codes::$Code1018[0], "ErrorMessage" => Codes::$Code1018[1]));
+				}
+				$AccountName = Account::where(['AccountName' => $data["AccountName"], 'CompanyID' => $CompanyID, 'AccountType' => 1])->count();
+				if ($AccountName > 0) {
+						return Response::json(["ErrorMessage" => Codes::$Code1029[1]], Codes::$Code410[0]);
+				}
+			}
+
+			if (isset($accountData['CurrencySymbol']) && !empty($accountData['CurrencySymbol'])) {
+				$data['CurrencyId'] = isset($accountData['CurrencySymbol']) ? $accountData['CurrencySymbol'] : '';
+			}
+			if (isset($accountData['CountryIso2']) && !empty($accountData['CountryIso2'])) {
+				$data['Country'] = isset($accountData['CountryIso2']) ? $accountData['CountryIso2'] : '';
+			}
+
+			if (isset($accountData['CustomerPanelPassword']) && !empty($accountData['CustomerPanelPassword'])) {
+				$data['password'] = isset($accountData['CustomerPanelPassword']) ? Crypt::encrypt($accountData['CustomerPanelPassword']) :'';
+			}
+
+			if (isset($accountData['VatNumber']) && !empty($accountData['VatNumber'])) {
+				$data['VatNumber'] = isset($accountData['VatNumber']) ? $accountData['VatNumber'] : '';
+			}
+			if (isset($accountData['LanguageIso2']) && !empty($accountData['LanguageIso2'])) {
+				$data['Language']= isset($accountData['LanguageIso2']) ? $accountData['LanguageIso2'] : '';
+			}
+
 			//when account varification is off in company setting then varified the account by default.
 			$AccountVerification =  CompanySetting::getKeyVal('AccountVerification');
 			if ( $AccountVerification != CompanySetting::ACCOUT_VARIFICATION_ON ) {
 				$data['VerificationStatus'] = Account::VERIFIED;
 			}
-			if (strpbrk($data['AccountName'], '\/?*:|"<>')) {
-				return Response::json(array("status" => Codes::$Code1018[0], "ErrorMessage" => Codes::$Code1018[1]));
-			}
 
-			Account::$APIrules['AccountName'] = 'required';
-			Account::$APIrules['Number'] = 'required';
-
-			$validator = Validator::make($data, Account::$APIrules, Account::$messages);
-
-			if ($validator->fails()) {
-				$errors = "";
-				foreach ($validator->messages()->all() as $error) {
-					$errors .= $error . "<br>";
+			if (!empty($data['CurrencyId'])) {
+				$data['CurrencyId'] = Currency::where('Symbol', $data['CurrencyId'])->pluck('CurrencyId');
+				if (!isset($data['CurrencyId'])) {
+					return Response::json(["ErrorMessage" => Codes::$Code1012[1], Codes::$Code1012[0]]);
 				}
-				return Response::json(["ErrorMessage" => $errors],Codes::$Code402[0]);
 			}
-
-			$AccountName = Account::where(['AccountName'=>$data["AccountName"],'CompanyID'=>$CompanyID,'AccountType'=>1])->count();
-			if ($AccountName > 0) {
-				return Response::json(["ErrorMessage"=>Codes::$Code1029[1]],Codes::$Code410[0]);
+			if (!empty($data['Country'])) {
+				$data['Country'] = Country::where(['ISO2' => $data['Country']])->pluck('Country');
+				if (!isset($data['Country'])) {
+					return Response::json(["ErrorMessage" => Codes::$Code1013[1]], Codes::$Code1013[0]);
+				}
 			}
-
-			$data['CurrencyId'] = Currency::where('Symbol',$data['CurrencyId'])->pluck('CurrencyId');
-			if (!isset($data['CurrencyId'])) {
-				return Response::json(["ErrorMessage" => Codes::$Code1012[1],Codes::$Code1012[0]]);
-			}
-			$data['Country'] = Country::where(['ISO2' => $data['Country']])->pluck('Country');
-			if (!isset($data['Country'])) {
-				return Response::json(["ErrorMessage" => Codes::$Code1013[1]],Codes::$Code1013[0]);
-			}
-
-			$data['LanguageID'] = Language::where('ISOCode',$data['Language'])->pluck('LanguageID');
-			if (!isset($data['LanguageID'])) {
-				return Response::json(["ErrorMessage" => Codes::$Code1014[1]],Codes::$Code1014[0]);
+			if (!empty($data['Language'])) {
+				$data['LanguageID'] = Language::where('ISOCode', $data['Language'])->pluck('LanguageID');
+				if (!isset($data['LanguageID'])) {
+					return Response::json(["ErrorMessage" => Codes::$Code1014[1]], Codes::$Code1014[0]);
+				}
 			}
 
 			if (!empty($BillingSetting['billing_class'])) {
