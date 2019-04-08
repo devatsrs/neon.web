d373c64264ac8ec27354dd6811b52570911a8ba8
Vasim Seta <vasim.seta@code-desk.com>
2019-04-08 18:38:10 +0530

    [change] [SI1-248] Ingenico card and bank detail store (https://codedesk.atlassian.net/browse/SI1-248)

---
+++ speak_inteligence_db/1rm/2.0/vasim.sql
@@ -0,0 +1,4 @@
+use `speakintelligentRM`;
+
+INSERT INTO `tblIntegration` (`IntegrationID`, `CompanyId`, `Title`, `Slug`, `ParentID`, `MultiOption`) VALUES (29, 1, 'Ingenico', 'ingenico', 4, 'N');
+
