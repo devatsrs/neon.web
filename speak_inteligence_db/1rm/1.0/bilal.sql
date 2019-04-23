d03a875e44bcedd10f6aceff1c72fd63e42339dd
bilalblaxi11 <bilalblaxi@gmail.com>
2019-04-23 12:08:34 +0500

    added insert cron job command query in sql file for updating pbx rate table for customer

--- speak_inteligence_db/1rm/1.0/bilal.sql
+++ speak_inteligence_db/1rm/1.0/bilal.sql
@@ -19,3 +19,5 @@ ALTER TABLE `tblAccountBalance`
 	ADD COLUMN `OutPaymentAwaiting` DECIMAL(18,6) NULL DEFAULT 0 AFTER `OutPayment`,
 	ADD COLUMN `OutPaymentAvailable` DECIMAL(18,6) NULL DEFAULT 0 AFTER `OutPaymentAwaiting`,
 	ADD COLUMN `OutPaymentPaid` DECIMAL(18,6) NULL DEFAULT 0 AFTER `OutPaymentAvailable`;
+
+INSERT INTO `tblCronJobCommand` (`CompanyID`, `GatewayID`, `Title`, `Command`, `Settings`, `Status`, `created_at`, `created_by`) VALUES ('1', NULL, 'Update PBX Customer Rate', 'updatepbxcustomerrate', '[[{"title":"Threshold Time (Minute)","type":"text","value":"","name":"ThresholdTime"},{"title":"Success Email","type":"text","value":"","name":"SuccessEmail"},{"title":"Error Email","type":"text","value":"","name":"ErrorEmail"}]]', '1', '2019-04-08 16:49:33', 'RateManagementSystem');
