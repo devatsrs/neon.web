7c211b07b340842e92ed745c8b841374262ca2fb
jenil ramavat <jenil.srs@gmail.com>
2019-05-30 18:27:38 +0530

    VOS - RatePrefix Changes while bulk mail send and download Ratesheet

--- app/models/CustomerRate.php
+++ app/models/CustomerRate.php
@@ -8,4 +8,14 @@ class CustomerRate extends \Eloquent {
     protected  $primaryKey = "CustomerRateID";
     public  $timestamps  = false;
 
+    public static function getRatePrefix($CustomerID=0){
+        if(empty($CustomerID)){
+            $CustomerRates = CustomerRate::select('RatePrefix')->whereNotNull('RatePrefix')->lists('RatePrefix','RatePrefix');
+        }else{
+            $CustomerRates = CustomerRate::where('CustomerID',$CustomerID)->whereNotNull('RatePrefix')->select('RatePrefix')->lists('RatePrefix','RatePrefix');
+        }
+        //print_r($CustomerRates);die;
+        return $CustomerRates;
+    }
+
 }
