1df5e708cb9ac323912119aeddaec019cd0dbd56
Haroon <haroonwaheed21@gmail.com>
2019-06-03 10:59:27 +0500

    update changes

--- app/controllers/DestinationGroupController.php
+++ app/controllers/DestinationGroupController.php
@@ -4,19 +4,18 @@ class DestinationGroupController extends \BaseController {
 
 
     public function index() {
-        $data = array();
         $CodedeckList = BaseCodeDeck::getCodedeckIDList();
-        $DestinationGroupActilead = UserActivity::UserActivitySaved($data,'View','DestinationGroup');
-
         return View::make('destinationgroup.index', compact('CodedeckList'));
     }
 
     public function ajax_datagrid(){
         $getdata = Input::all();
-        $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Search','DestinationGroup');
+        $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'View','Destination Group Set');
         $response =  NeonAPI::request('destinationgroupset/datagrid',$getdata,false,false,false);
         if(isset($getdata['Export']) && $getdata['Export'] == 1 && !empty($response) && $response->status == 'success') {
-                $excel_data = $response->data;
+            $export_type['type'] = 'xls';
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($export_type,'Export','Destination Group Set');    
+            $excel_data = $response->data;
                 $excel_data = json_decode(json_encode($excel_data),true);
                 Excel::create('Destination Group', function ($excel) use ($excel_data) {
                     $excel->sheet('Destination Group', function ($sheet) use ($excel_data) {
@@ -29,21 +28,33 @@ class DestinationGroupController extends \BaseController {
     public function store(){
         $postdata = Input::all();
         $response =  NeonAPI::request('destinationgroupset/store',$postdata,true,false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Add','Destination Group Set',$postdata['Name']);
+        }
         return json_response_api($response);
     }
         public function delete($id){
+        $data['id'] = $id;
         $response =  NeonAPI::request('destinationgroupset/delete/'.$id,array(),'delete',false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($data,'Delete','Destination Group Set');
+        }
         return json_response_api($response);
     }
     public function update($id){
         $postdata = Input::all();
         $response =  NeonAPI::request('destinationgroupset/update/'.$id,$postdata,'put',false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Edit','Destination Group Set',$postdata['Name']);
+        }
         return json_response_api($response);
     }
     public function show($id) {
+        $data['id'] = $id;
         $DestinationGroupSetID = $id;
         $name = DestinationGroupSet::getName($id);
         $discountplanapplied = DiscountPlan::isDiscountPlanApplied('DestinationGroupSet',$id,0);
+        $DestinationGroupActilead = UserActivity::UserActivitySaved($data,'View','Destination Group Set');
         return View::make('destinationgroup.show', compact('DestinationGroupSetID','countries','name','discountplanapplied'));
     }
     public function group_show($id) {
@@ -58,8 +69,12 @@ class DestinationGroupController extends \BaseController {
 
     public function group_ajax_datagrid(){
         $getdata = Input::all();
+        $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'View','Destination Group');
         $response =  NeonAPI::request('destinationgroup/datagrid',$getdata,false,false,false);
+        
         if(isset($getdata['Export']) && $getdata['Export'] == 1 && !empty($response) && $response->status == 'success') {
+            $export_type['type'] = 'xls';
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($export_type,'Export','Destination Group');
             $excel_data = $response->data;
             $excel_data = json_decode(json_encode($excel_data), true);
             Excel::create('Destination Group Set', function ($excel) use ($excel_data) {
@@ -96,10 +111,17 @@ class DestinationGroupController extends \BaseController {
             $postdata['Description'] = $postdata['FilterDescription'];
         }
         $response =  NeonAPI::request('destinationgroup/store',$postdata,true,false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Add','Destination Group',$postdata['Name']);
+        }
         return json_response_api($response);
     }
     public function group_delete($id){
+        $getdata['id'] = $id; 
         $response =  NeonAPI::request('destinationgroup/delete/'.$id,array(),'delete',false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Delete','Destination Group');
+        }
         return json_response_api($response);
     }
     public function group_update($id){
@@ -114,6 +136,9 @@ class DestinationGroupController extends \BaseController {
             $postdata['Description'] = $postdata['FilterDescription'];
         }
         $response =  NeonAPI::request('destinationgroup/update/'.$id,$postdata,'put',false,false);
+        if($response->status == 'success'){
+            $DestinationGroupActilead = UserActivity::UserActivitySaved($getdata,'Edit','Destination Group',$postdata['Name']);
+        }
         return json_response_api($response);
     }
     public function update_name($id){
