ef9fd1526be24e7cfcdce575c9ca7b30d4e3968c
Vasim Seta <vasim.seta@code-desk.com>
2019-06-11 18:17:26 +0530

    [fixed] When mapping by column number while uploading dual mapping was creating issue (currency,city,tariff etc...)

--- app/models/ServiceTemplate.php
+++ app/models/ServiceTemplate.php
@@ -70,8 +70,11 @@ class ServiceTemplate extends \Eloquent
         return '';
     }
 
-    public static function getAccessTypeDD($CompanyID){
-        return ServiceTemplate::where("CompanyID",$CompanyID)->where("accessType",'!=','')->orderBy('accessType')->lists("accessType", "accessType");
+    public static function getAccessTypeDD($CompanyID,$includePrefix=0){
+        if($includePrefix == 1)
+            return ServiceTemplate::select('accessType',DB::raw('CONCAT("DBDATA-",accessType) AS accessTypeValue'))->where("CompanyID",$CompanyID)->where("accessType",'!=','')->orderBy('accessType')->lists("accessType", "accessTypeValue");
+        else
+            return ServiceTemplate::where("CompanyID",$CompanyID)->where("accessType",'!=','')->orderBy('accessType')->lists("accessType", "accessType");
     }
     public static function verifyAccessTypeDD($CompanyID,$accessType){
         return ServiceTemplate::where("CompanyID",$CompanyID)->where("accessType",'=',$accessType)->count();
@@ -86,17 +89,29 @@ class ServiceTemplate extends \Eloquent
         return ServiceTemplate::where("CompanyID",$CompanyID)->where("Tariff",'=',$Tariff)->count();
     }
 
-    public static function getPrefixDD($CompanyID){
-        return ServiceTemplate::where("CompanyID",$CompanyID)->where("prefixName",'!=','')->orderBy('prefixName')->lists("prefixName", "prefixName");
+    public static function getPrefixDD($CompanyID,$includePrefix=0){
+        if($includePrefix == 1)
+            return ServiceTemplate::select('prefixName',DB::raw('CONCAT("DBDATA-",prefixName) AS prefixNameValue'))->where("CompanyID",$CompanyID)->where("prefixName",'!=','')->orderBy('prefixName')->lists("prefixName", "prefixNameValue");
+        else
+            return ServiceTemplate::where("CompanyID",$CompanyID)->where("prefixName",'!=','')->orderBy('prefixName')->lists("prefixName", "prefixName");
     }
-    public static function getCityDD($CompanyID){
-        return ServiceTemplate::where("CompanyID",$CompanyID)->where("City",'!=','')->orderBy('City')->lists("City", "City");
+    public static function getCityDD($CompanyID,$includePrefix=0){
+        if($includePrefix == 1)
+            return ServiceTemplate::select('City',DB::raw('CONCAT("DBDATA-",City) AS CityValue'))->where("CompanyID",$CompanyID)->where("City",'!=','')->orderBy('City')->lists("City", "CityValue");
+        else
+            return ServiceTemplate::where("CompanyID",$CompanyID)->where("City",'!=','')->orderBy('City')->lists("City", "City");
     }
-    public static function getTariffDD($CompanyID){
-        return ServiceTemplate::where("CompanyID",$CompanyID)->where("Tariff",'!=','')->orderBy('Tariff')->lists("Tariff", "Tariff");
+    public static function getTariffDD($CompanyID,$includePrefix=0){
+        if($includePrefix == 1)
+            return ServiceTemplate::select('Tariff',DB::raw('CONCAT("DBDATA-",Tariff) AS TariffValue'))->where("CompanyID",$CompanyID)->where("Tariff",'!=','')->orderBy('Tariff')->lists("Tariff", "TariffValue");
+        else
+            return ServiceTemplate::where("CompanyID",$CompanyID)->where("Tariff",'!=','')->orderBy('Tariff')->lists("Tariff", "Tariff");
     }
-    public static function getCountryPrefixDD(){
-        return $country = Country::select('Country AS country','Prefix')->orderBy('country')->lists("country", "Prefix");
+    public static function getCountryPrefixDD($includePrefix=0){
+        if($includePrefix == 1)
+            return $country = Country::select('Country AS country',DB::raw('CONCAT("DBDATA-",Prefix) AS Prefix'))->orderBy('country')->lists("country", "Prefix");
+        else
+            return $country = Country::select('Country AS country','Prefix')->orderBy('country')->lists("country", "Prefix");
     }
     public static function getCountryDD($CompanyID){
         $country = ServiceTemplate::Join('tblCountry', function($join) {
