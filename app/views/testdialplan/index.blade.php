4821ec67ae2335cf17d228d08cec7a5cd3fd15fd
aamarnazir <aamarnazir@code-desk.com>
2019-06-18 01:50:49 -0700

    SI1-315 - big fixing in SI

--- app/views/testdialplan/index.blade.php
+++ app/views/testdialplan/index.blade.php
@@ -34,7 +34,7 @@
                             <input type="text" name="StartDate"  class="form-control datepicker"  data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}" data-enddate="{{date('Y-m-d')}}"/>
                         </div>
                         <div class="col-md-6 select_hour">
-                            <input type="text" name="StartHour" data-minute-step="30"   data-show-meridian="false" data-default-time="00:00" value="{{date('H:i')}}"  data-template="dropdown" class="form-control timepicker">
+                            <input type="text" name="StartHour" data-minute-step="30"   data-show-meridian="false" data-default-time="00:00" value=""  data-template="dropdown" class="form-control timepicker">
                         </div>
                     </div>
                 </div>
@@ -101,7 +101,11 @@
         $searchFilter.SelectedCodes = '';
         var code_check = 0;
         var first_call = true;
+
+
         $(function () {
+            var d = new Date();
+            $("#testdialplan_form [name='StartHour']").val(d.getHours() + ":" + d.getMinutes());
             //load_vendorGrid();
 $('#filter-button-toggle').show();
             $('.nav-tabs li a').click(function (e) {
