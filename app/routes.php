b3af444f9098ad2d1f2499276a9ab82f41314965
bilalblaxi11 <bilalblaxi@gmail.com>
2019-07-29 14:31:24 +0500

    added deal management view and controller

--- app/routes.php
+++ app/routes.php
@@ -812,6 +812,16 @@ Route::group(array('before' => 'auth'), function () {
 	Route::any('lcr/ajax_customer_rate_export/{type}', 'LCRController@ajax_customer_rate_export');
 	Route::any('lcr/edit_preference', 'LCRController@editPreference');
 
+	//Deal Management
+	Route::any('/dealmanagement', 'DealManagementController@index');
+	Route::any('dealmanagement/ajax_datagrid', 'DealManagementController@ajax_datagrid');
+	Route::any('dealmanagement/exports/{type}', 'DealManagementController@ajax_datagrid');
+	Route::any('dealmanagement/create', "DealManagementController@create");
+	Route::any('dealmanagement/store','DealManagementController@store');
+	Route::any('dealmanagement/{id}/delete', 'DealManagementController@delete');
+	Route::any('dealmanagement/{id}/edit', 'DealManagementController@edit');
+	Route::any('dealmanagement/{id}/update', "DealManagementController@update");
+
 	//Pages
 	Route::any('/about', 'PagesController@about');
 	Route::resource('page', 'PagesController');
