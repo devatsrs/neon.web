4d76aef8ab12d251a8ff9c8e3219131531f7a03a
shamw4r <ahtshamb4u@hotmail.com>
2019-08-02 14:57:06 +0500

    add two more options

--- app/views/report/create.blade.php
+++ app/views/report/create.blade.php
@@ -458,6 +458,10 @@
                                             ?>
                                             <label for="field-5" class="control-label">Date Selection</label>
                                             <select class="select2 small" name="date_range_filter" id="date_range_filter">
+                                                <option value="Today" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='Today'){echo 'selected';}?>>Today</option>
+                                                <option value="1Days" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='1Days'){echo 'selected';}?>>2 Days</option>
+                                                <option value="2Days" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='1Days'){echo 'selected';}?>>4 Days</option>
+                                                
                                                 <option value="1Week" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='1Week'){echo 'selected';}?>>1 Week</option>
                                                 <option value="2Week" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='2Week'){echo 'selected';}?>>2 Week</option>
                                                 <option value="1Month" <?php if(isset($filter_settings['date']['date_range_filter']) && $filter_settings['date']['date_range_filter']=='1Month'){echo 'selected';}?>>1 Month</option>
--- app/views/report/script.blade.php
+++ app/views/report/script.blade.php
@@ -23,6 +23,12 @@
     var column_function = {'top5':'Top 5', 'top10':'Top 10','bottom5':'Bottom 5','bottom10':'Bottom 10','':'Actual','min':'Min','max':'Max','sum':'Sum','avg':'Average','count':'Count','count_distinct':'Count Distinct'};
     function getLastDates(duration) {
         var today = new Date();
+        if(duration == '1Days'){
+            var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+        }
+        if(duration == '2Days'){
+            var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 2);
+        }
         if(duration == '1Week'){
             var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
         }
@@ -44,7 +50,7 @@
         if(duration == '1Year'){
             var lastWeek = new Date(today.getFullYear(), today.getMonth() - 12, today.getDate()); 
         }
-        if(duration == 'Custom'){
+        if(duration == 'Custom' || duration == 'Today'){
             var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate()); 
         }
         return lastWeek;
