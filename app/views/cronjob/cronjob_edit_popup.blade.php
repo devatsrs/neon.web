9b90c391ab27465ec74caaef3f807c1455b142ac
Haroon <haroon.waheed.codedesk@gmail.com>
2019-10-16 19:09:39 +0500

    fix the ordering of nodes in cron job screen

--- app/views/cronjob/cronjob_edit_popup.blade.php
+++ app/views/cronjob/cronjob_edit_popup.blade.php
@@ -30,7 +30,7 @@
             submit_ajax(baseurl+'/cronjobs/delete/'+$(this).attr('data-id'));
         }
     });
-    // var nodesArr = '{{ json_encode($Nodes) }}';
+    var nodesArr = '{{ json_encode($Nodes) }}';
     $('table tbody').on('click','.edit-config',function(ev){
         ev.preventDefault();
         ev.stopPropagation();
@@ -64,39 +64,31 @@
             }
             if(json !== null && typeof  json['JobStartDay'] != 'undefined'){
                 $("#add-new-config-form [name='Setting[JobStartDay]']").val(json['JobStartDay']).trigger("change");
-            }
-
-           
+            }   
             if(json !== null && typeof  json['Nodes'] != 'undefined'){
-
-                // var nodesarray = json['Nodes'];
-				// console.log($('#json_nodes').val());
-                // var y = JSON.parse(nodesArr);
-                // var x = {}; 
-                // $.each(nodesarray,(i,j) => {
-                //     $.each(y,(a,b) => {
-                //         if(j == a){
-                //             x[i + " - " + a] = b;
-				// 			console.log(y[a]);
-                //             delete y[a];
-                //         }
-                //     });
-                // });
-
-				// var z = $.extend(x,y);
-                // console.log(z);
-				// $("#add-new-config-form [name='Setting[Nodes][]']").empty();
-				// var options = "";
-				// var counter = 0;
-				// $.each(z, function(item, val){
-				// 	item = item.replace(counter + " - ", "");
-				// 	options += "<option value=" + item + ">" + val + "</option>";
-				// 	counter++;
-				// });
-				// $("#add-new-config-form [name='Setting[Nodes][]']").html(options);
-				// $("#add-new-config-form [name='Setting[Nodes][]']").trigger("change");
-                // $("#add-new-config-form [name='Setting[Nodes][]']").val(json['Nodes']).trigger("change");
-
+                var nodesarray = json['Nodes'];
+                var y = JSON.parse(nodesArr);
+                var x = {}; 
+                $.each(nodesarray,(i,j) => {
+                    $.each(y,(a,b) => {
+                        if(j == a){
+                            x[i + " - " + a] = b;
+							console.log(y[a]);
+                            delete y[a];
+                        }
+                    });
+                });
+				var z = $.extend(x,y);
+                console.log(z);
+				$("#add-new-config-form [name='Setting[Nodes][]']").empty();
+				var options = "";
+				$.each(z, function(item, val){
+                if(item.lastIndexOf(" - ") > 0)
+                    item = item.splice(0, item.lastIndexOf(" "));
+                options += "<option value=" + item + ">" + val + "</option>";
+                });
+				$("#add-new-config-form [name='Setting[Nodes][]']").html(options);
+				$("#add-new-config-form [name='Setting[Nodes][]']").trigger("change");
                 $("#add-new-config-form [name='Setting[Nodes][]']").val(json['Nodes']).trigger("change");
             }else{
                 $("#add-new-config-form [name='Setting[Nodes][]']").val('').trigger("change");
