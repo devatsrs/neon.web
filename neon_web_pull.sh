eb3b7936bacc26517f8b14c2c7fab7b9cab00f3b
Dev <shriramsoft@gmail.com>
2019-12-10 11:55:07 +0530

    neon web pull

--- neon_web_pull.sh
+++ neon_web_pull.sh
@@ -11,7 +11,7 @@ sudo chmod -R 770 /vol/data/speakintelligent.neon/app/storage
 sudo chmod -R 770 /vol/data/speakintelligent.neon/public/neon.api/storage
 
 sudo chmod -R 575 /vol/data/speakintelligent.neon/wkhtmltox/bin/wkhtmltopdf
-
+sudo chmod -R 575 /vol/data/speakintelligent.neon/wkhtmltox/bin/wkhtmltopdf
 
 #sudo chown -R neon_sys:apache /vol/data/tmp
 #sudo find /vol/data/tmp -type d -exec chmod 775 {} +
