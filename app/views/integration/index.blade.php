009b532492a04d7cbe269722694d2f93ef329556
imran0021 <imran.codedesk@gmail.com>
2020-01-31 15:53:06 +0500

    change ids

--- app/views/integration/index.blade.php
+++ app/views/integration/index.blade.php
@@ -1092,8 +1092,8 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label class="control-label">Active:</label>
-                                    <div id="gocardlessStatusDiv">
-                                        <input id="GoCardLessStatus" class="subcatstatus" Divid="gocardlessStatusDiv" name="Status" type="checkbox" value="1" <?php if(isset($masavData->Status) && $masavData->Status==1){ ?>   checked="checked"<?php } ?> >
+                                    <div id="masavStatusDiv">
+                                        <input id="masavStatus" class="subcatstatus" Divid="masavStatusDiv" name="Status" type="checkbox" value="1" <?php if(isset($masavData->Status) && $masavData->Status==1){ ?>   checked="checked"<?php } ?> >
                                     </div>
                                 </div>
                             </div>
